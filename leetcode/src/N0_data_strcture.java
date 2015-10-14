ac0d79b02c53ffd818132adaa5ce02350ef16cc1
zerghua <henry.z.cn@gmail.com>
2015-10-13 22:03:06 -0400

    first try

---
+++ leetcode/src/N0_data_strcture.java
@@ -0,0 +1,17 @@
+public class N0_data_strcture {
+	// Definition for singly-linked list.
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	//Definition for a binary tree node.
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) { val = x; }
+	}
+	
+}
