05443806d3c9376ec8361ae11a124a59c82d18f5
hua_mac <zerghua@gmail.com>
2015-10-25 00:03:07 -0400

    commit from mac

--- leetcode/src/leetcode/N0_data_strcture.java
+++ leetcode/src/leetcode/N0_data_strcture.java
@@ -1,32 +1,32 @@
 package leetcode;
 
 public class N0_data_strcture {
-	// Definition for singly-linked list.
-	public static class ListNode {
-		int val;
-		ListNode next;
-		ListNode(int x) { val = x; }
-	}
-	
-	//Definition for a binary tree node.
-	public static class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-		TreeNode(int x) { val = x; }
-	}
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 
-	public class TreeLinkNode {
-		int val;
-		TreeLinkNode left, right, next;
+    //Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
 
-		TreeLinkNode(int x) {
-			val = x;
-		}
-	}
-	// add a comment
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
 
-	// test on mac
-	
-	// test on PC
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
+    // add a comment
+
+    // test on mac
+
+    // test on PC
 }
