28d9f7e29737b1b5041386d3a1292841d62f410e
zerghua <zerghua@gmail.com>
2015-10-29 20:56:26 -0400

    89, gray code, https://en.wikipedia.org/wiki/Gray_code

---
+++ leetcode/src/leetcode/N89_GrayCode.java
@@ -0,0 +1,36 @@
+package leetcode;
+import java.util.*;
+
+/**
+The gray code is a binary numeral system where two successive values differ in only one bit.
+
+ Given a non-negative integer n representing the tota l number of bits in the code,
+ print the sequence of gray code. A gray code sequence must begin with 0.
+
+ For example, given n = 2, ret urn [0,1,3,2]. Its gray code sequence is:
+
+ 00 - 0
+ 01 - 1
+ 11 - 3
+ 10 - 2
+
+ * n=3, result expects: [0,1,3,2,6,7,5,4]
+ * https://en.wikipedia.org/wiki/Gray_code
+ *
+ */
+public class N89_GrayCode {
+    //1 ms
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(0);
+        int k=1;
+        for(int i=1; i<=n; i++){
+            int ret_length=ret.size();
+            for(int j=ret_length-1;j>=0;j--){
+               ret.add(ret.get(j)+k);
+            }
+            k <<= 1;
+        }
+        return ret;
+    }
+}
