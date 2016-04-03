f1db18313641dcf951291eb00e689113ce5975ca
Hua Z <zerghua@gmail.com>
2016-04-02 21:37:43 -0400

    5, checking palindrome start from [center,center] and [center, center+1]

---
+++ leetcode/src/leetcode/N5_LongestPalindromicSubstring.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+/**
+ * Created by Hua on 4/2/2016.
+ */
+public class N5_LongestPalindromicSubstring {
+    //38 ms, time O(n^2), space O(1)
+    public String longestPalindrome(String s) {
+        if(s==null || s.length()==1) return s;
+
+        String longest="";
+        for(int i=0;i<s.length();i++){
+            String tmp = findPalindromeAroundCenter(s,i,i);
+            if(tmp.length() > longest.length()) longest = tmp;
+
+            tmp = findPalindromeAroundCenter(s,i,i+1);
+            if(tmp.length() > longest.length()) longest = tmp;
+        }
+        return longest;
+    }
+
+    private String findPalindromeAroundCenter(String s, int start, int end) {
+        while(start>=0 && end<s.length() ){
+            if(s.charAt(start) == s.charAt(end)){
+                start--;
+                end++;
+            }else break;
+        }
+        return s.substring(start+1, end);
+    }
+
+
+    //DP 66 ms time O(n^2), space O(n^2)
+    public String longestPalindrome2(String s) {
+        if(s==null || s.length()==1) return s;
+
+        int start=0, end=0, max_len=0;
+        boolean[][] isPal = new boolean[s.length()][s.length()];
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=i;j<s.length();j++){
+                if( s.charAt(i) == s.charAt(j) && (j-i<=1 || isPal[i+1][j-1])) {
+                    isPal[i][j] = true;
+                    if (j - i + 1 > max_len) {
+                        max_len = j - i + 1;
+                        start = i; end = j;
+                    }
+                }
+            }
+        }
+        return s.substring(start,end+1);
+    }
+
+}
