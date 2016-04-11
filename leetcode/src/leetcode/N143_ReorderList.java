d4a066332c030479b7c9819b443b5630515903ea
Hua Z <zerghua@gmail.com>
2016-04-11 19:12:52 -0400

    143, linked list, find mid point, reverse list, merge list

---
+++ leetcode/src/leetcode/N143_ReorderList.java
@@ -0,0 +1,48 @@
+package leetcode;
+import leetcode.N0_data_strcture.*;
+/**
+ * Created by Hua on 4/11/2016.
+ */
+public class N143_ReorderList {
+    // find mid point
+    // reverse second half list
+    // merge two list
+
+    //3 ms
+    public void reorderList(ListNode head) {
+        ListNode right_end = reverseList(findMidPoint(head));
+        mergeList(head, right_end);
+    }
+
+    public void mergeList(ListNode left, ListNode right){
+        ListNode cur = left;
+        while(right!=null && right.next != null) {
+            ListNode tmp = right.next;
+            right.next = cur.next;
+            cur.next = right;
+            cur = cur.next.next;
+            right = tmp;
+        }
+    }
+
+    public ListNode findMidPoint(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode head){
+        ListNode pre = null, cur = head, next=cur;
+        while(cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
