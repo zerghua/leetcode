8a7b03ad0659a143f98ddcfaa229f821c576a85e
Hua Z <zerghua@gmail.com>
2016-04-21 20:49:42 -0400

    72, 2-dimensional dp, dp[i][j] determines by its top, left and top-left.

---
+++ leetcode/src/leetcode/N72_EditDistance.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * Created by Hua on 4/21/2016.
+ */
+public class N72_EditDistance {
+    //18 ms
+    // 2-dimensional dp, dp[i][j] determines by its top, left and top-left.
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+
+        for(int i=0;i<=word1.length();i++) dp[i][0] = i;
+        for(int i=0;i<=word2.length();i++) dp[0][i] = i;
+
+        for(int i=0;i<word1.length();i++){
+            char c1 = word1.charAt(i);
+            for(int j=0;j<word2.length();j++){
+                char c2 = word2.charAt(j);
+                if(c1 == c2) dp[i+1][j+1] = dp[i][j];
+                else{
+                    int insert = dp[i+1][j] + 1;
+                    int delete = dp[i][j+1] + 1;
+                    int replace = dp[i][j] + 1;
+                    dp[i+1][j+1] = Math.min(Math.min(delete, replace),insert);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+}
