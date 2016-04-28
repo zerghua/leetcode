57ae0a53bfffcec5ab1c159ad8c4253a6aa3f9f7
Hua Z <zerghua@gmail.com>
2016-04-27 20:15:07 -0400

    138, deep copy list. insert new node after original and separate the list to two.

---
+++ leetcode/src/leetcode/N138_CopyListwithRandomPointer.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * Created by Hua on 4/27/2016.
+ */
+public class N138_CopyListwithRandomPointer {
+    public class RandomListNode {
+        int label;
+        RandomListNode next, random;
+        RandomListNode(int x) {
+            this.label = x;
+        }
+    }
+
+    //2 ms
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null) return null;
+
+        // make copy of list and link them, as A->A1->B->B1
+        RandomListNode cur = head;
+        while(cur != null){
+            RandomListNode newNode = new RandomListNode(cur.label);
+            RandomListNode cur_next = cur.next;
+            cur.next = newNode;
+            newNode.next = cur_next;
+            cur = cur_next;
+        }
+
+        // make new node's random point to new copy's random
+        cur = head;
+        while(cur != null){
+            if(cur.random != null){
+                cur.next.random = cur.random.next; //important, new node's random
+            }else cur.next.random = null;
+            cur = cur.next.next;
+        }
+
+        //separate two list
+        RandomListNode new_head = head.next;
+        cur = head;
+        while(cur !=null && cur.next != null){
+            RandomListNode cur_next = cur.next;
+            cur.next = cur_next.next;
+            cur = cur_next;
+        }
+        return new_head;
+    }
+}
