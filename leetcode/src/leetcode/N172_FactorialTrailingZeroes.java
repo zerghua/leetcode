7e2dd29ea58b62978c51c040d26bc0fb1f5071da
Hua Z <zerghua@gmail.com>
2016-05-21 10:29:09 -0400

    172, trailing zeros, count number of 5s.

---
+++ leetcode/src/leetcode/N172_FactorialTrailingZeroes.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * Created by Hua on 5/21/2016.
+
+ Given an integer n, return the number of trailing zeroes in n!.
+
+ Note: Your solution should be in logarithmic time complexity.
+
+ */
+public class N172_FactorialTrailingZeroes {
+    //1 ms
+    //count number of 5s, equal trailing zeros
+    public int trailingZeroes(int n) {
+        int ret = 0;
+        while(n>0){
+            int k=n/5;
+            ret += k;
+            n=k;
+        }
+        return ret;
+    }
+}
