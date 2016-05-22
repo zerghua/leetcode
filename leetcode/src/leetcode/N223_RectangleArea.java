3194ecd006f535eb0267413dc6e22716ec412820
Hua Z <zerghua@gmail.com>
2016-05-22 11:02:31 -0400

    223, math, easy. get rectangle area.

---
+++ leetcode/src/leetcode/N223_RectangleArea.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by Hua on 5/22/2016.
+
+ Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+ Each rectangle is defined by its bottom left corner and top right corner
+ as shown in the figure.
+
+
+ Assume that the total area is never beyond the maximum possible value of int.
+
+
+ */
+public class N223_RectangleArea {
+    // 4 ms
+    // math
+    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        // no overlap
+        if(G<A || C<E || D<F || H<B) return (C-A)*(D-B) + (G-E)*(H-F);
+
+        //overlap
+        int left=Math.max(A,E);
+        int right=Math.min(C,G);
+        int top = Math.min(D,H);
+        int bottom = Math.max(B,F);
+
+        return (C-A)*(D-B) + (G-E)*(H-F) - (right-left)*(top-bottom);
+    }
+
+}
