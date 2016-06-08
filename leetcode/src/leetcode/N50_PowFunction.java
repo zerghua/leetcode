e6852547fe9a094f1d7f25ab0f76b61e441a2086
Hua Z <zerghua@gmail.com>
2016-06-07 20:42:25 -0400

    50, implement pow function, corner case n=-2147483648 (Integer.MIN_VALUE) will cause overflow when set n= -n, see code for solution.

---
+++ leetcode/src/leetcode/N50_PowFunction.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by Hua on 6/7/2016.
+
+ Implement pow(x, n).
+
+ */
+public class N50_PowFunction {
+    /*
+    // java.lang.StackOverflowError with input
+    // 1.00000   or 2.00000
+    // -2147483648
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        double ret = myPowHelper(x, Math.abs(n));
+        if(n<0) ret = 1.0/ret;
+        return ret;
+    }
+
+    private double myPowHelper(double x, int n) {
+        if(n==1) return x;
+        int mid = n/2;
+        double ret = myPowHelper(x,mid);
+        ret *= ret;
+        if(n%2 == 1) ret *= x;
+        return ret;
+    }
+
+    // iterative
+    // Memory Limit Exceeded
+    // 1.00000   or 2.00000
+    // -2147483648
+    public double myPow2(double x, int n) {
+        if(n == 0) return 1;
+        Stack<Integer> stack = new Stack<>();
+        int m = Math.abs(n);
+        while(m!=1){
+            stack.add(m%2);
+            m/=2;
+        }
+        double ret =x;
+        while(!stack.isEmpty()){
+            ret *=ret;
+            if(stack.pop() == 1) ret*=x;
+        }
+
+        if(n<0) ret = 1.0/ret;
+        return ret;
+    }
+*/
+
+    //2 ms
+    // corner case: n=-2147483648 Integer.MIN_VALUE
+    // solution for corner case: x*myPwo(x, -(n+1))
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        //if(n<0) return 1/myPow(x,-n); // this line will cause stack overflow when n=-2147483648
+        if(n<0) return 1/(x*myPow(x,-(n+1)));
+
+        double ret = myPow(x,n/2);
+        if(n%2 == 0) return ret*ret;
+        else return ret*ret*x;
+    }
+
+}
