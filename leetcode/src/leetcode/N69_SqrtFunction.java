024b371c238941dce3c381cf5c1723bbf3137665
Hua Z <zerghua@gmail.com>
2016-06-08 18:07:06 -0400

    69, binary search, corner case Integer.MAX_VALUE, need to use long type rather than int for mid*mid, or will overflow.

---
+++ leetcode/src/leetcode/N69_SqrtFunction.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+/**
+ * Created by Hua on 6/8/2016.
+
+ Implement int sqrt(int x).
+
+ Compute and return the square root of x.
+
+ */
+public class N69_SqrtFunction {
+    // TLE on 2147483647?
+    // need to use long type for mid*mid, or possibly over flow.
+    // 3 ms
+    public int mySqrt(int x) {
+        if(x<=1) return x; // it seems OJ treat this way, not mathematically right
+
+        int ret=1, left=1, right=x;
+        while(left<=right){
+            int mid = (right-left)/2 + left;
+            long squareOfMid = (long)mid*mid;
+            if(squareOfMid == x) return mid;
+            else if(squareOfMid < x){
+                ret = mid;
+                left = mid+1;
+            }else right = mid-1;
+        }
+        return ret;
+    }
+}
