99653592f9bbc8b8d570c4b7772bbd601f2dd24c
Hua Z <zerghua@gmail.com>
2016-07-31 18:05:42 -0400

    3, add problem description.

--- leetcode/src/leetcode/N3_lengthOfLongestSubstring.java
+++ leetcode/src/leetcode/N3_lengthOfLongestSubstring.java
@@ -1,8 +1,22 @@
 package leetcode;
 import java.util.HashMap;
 
+/*
+    Given a string, find the length of the longest substring without repeating characters.
+
+    Examples:
+
+    Given "abcabcbb", the answer is "abc", which the length is 3.
+
+    Given "bbbbb", the answer is "b", with the length of 1.
+
+    Given "pwwkew", the answer is "wke", with the length of 3.
+    Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+ */
 public class N3_lengthOfLongestSubstring {
 	//425ms 38%
+    // sliding window, o(n)
     public int lengthOfLongestSubstring(String s) {
         int max=0;
         int lastIndex=-1;
