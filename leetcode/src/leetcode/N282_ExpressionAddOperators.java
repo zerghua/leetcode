9ffdf4093ab6d94ad474d50ef52f3dc33943fb8c
zerghua <zerghua@gmail.com>
2016-08-07 16:03:35 -0400

    282, 3 DFS.

---
+++ leetcode/src/leetcode/N282_ExpressionAddOperators.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * Created by Hua on 8/7/16.
+
+ Given a string that contains only digits 0-9 and a target value,
+ return all possibilities to add binary operators (not unary) +, -, or * between
+ the digits so they evaluate to the target value.
+
+
+ Examples:
+
+ "123", 6 -> ["1+2+3", "1*2*3"]
+ "232", 8 -> ["2*3+2", "2+3*2"]
+ "105", 5 -> ["1*0+5","10-5"]
+ "00", 0 -> ["0+0", "0-0", "0*0"]
+ "3456237490", 9191 -> []
+
+
+ */
+public class N282_ExpressionAddOperators {
+    // 286 ms DFS
+    public List<String> addOperators(String num, int target) {
+        List<String> ret = new ArrayList<>();
+        dfs(ret, num, target, "", 0, 0, 0);
+        return ret;
+    }
+
+    public void dfs(List<String> ret, String num, int target, String path, int pos, long sum, long lastNum){
+        if(pos == num.length()){
+            if(sum == target) ret.add(path);
+            return;
+        }
+        for(int i=pos; i<num.length();i++){
+            if(num.charAt(pos) == '0' && pos != i) break; // skip corner case like 000
+            long cur = Long.parseLong(num.substring(pos,i+1));
+            if(pos == 0) dfs(ret, num, target, path + cur, i+1, cur, cur);//first num, has to be i+1
+            else{
+                dfs(ret, num, target, path + "+" + cur, i+1, sum+cur, cur);
+                dfs(ret, num, target, path + "-" + cur, i+1, sum-cur, -cur);
+                dfs(ret, num, target, path + "*" + cur, i+1, sum-lastNum + lastNum*cur, lastNum*cur);
+            }
+        }
+    }
+}
