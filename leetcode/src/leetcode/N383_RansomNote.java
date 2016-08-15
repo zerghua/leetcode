3e74b8e9a43c3cd1b981503949ee178b7579a45b
zerghua <zerghua@gmail.com>
2016-08-15 19:10:31 -0400

    383, hash table, char count.

---
+++ leetcode/src/leetcode/N383_RansomNote.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by HuaZ on 8/15/2016.
+
+ Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from 
+ all  the  magazines,  write  a  function  that  will  return  true  if  the  ransom   note  can  
+ be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
+
+ Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+
+ Note:
+ You may assume that both strings contain only lowercase letters.
+
+ canConstruct("a", "b") -> false
+ canConstruct("aa", "ab") -> false
+ canConstruct("aa", "aab") -> true
+
+
+ */
+public class N383_RansomNote {
+    // 14 ms
+    // hash table
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] mag = new int[128];
+        for(char c: magazine.toCharArray()) mag[c]++;
+        for(char c: ransomNote.toCharArray()){
+            if(--mag[c] < 0) return false;
+        }
+        return true;
+    }
+}
