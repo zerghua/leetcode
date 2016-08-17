d2822c4edcdcff434785e48684680fbae70bf08d
zerghua <zerghua@gmail.com>
2016-08-16 20:20:26 -0400

    384, random?

---
+++ leetcode/src/leetcode/N384_ShuffleanArray.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+import java.util.Random;
+
+/**
+ * Created by HuaZ on 8/16/2016.
+
+ Shuffle a set of numbers without duplicates.
+
+ Example:
+
+ // Init an array with set 1, 2, and 3.
+ int[] nums = {1,2,3};
+ Solution solution = new Solution(nums);
+
+ // Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3]
+ // must equally likely to be returned.
+ solution.shuffle();
+
+ // Resets the array back to its original configuration [1,2,3].
+ solution.reset();
+
+ // Returns the random shuffling of array [1,2,3].
+ solution.shuffle();
+
+
+ */
+public class N384_ShuffleanArray {
+    // 311 ms
+    // random ?
+    public class Solution {
+        private int[] nums;
+        private Random random;
+
+        public Solution(int[] nums) {
+            this.nums = nums;
+            random = new Random();
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] reset() {
+            return this.nums;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] shuffle() {
+            int[] a = nums.clone();
+            for(int i=1;i<a.length;i++){
+                int j = random.nextInt(i+1);
+                swap(a, i, j);
+            }
+            return a;
+        }
+
+        private void swap(int[] a, int i, int j){
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+    }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+}
