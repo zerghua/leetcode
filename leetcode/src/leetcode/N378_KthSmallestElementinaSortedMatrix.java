355eab3d8334dc868b955f0ae28e02835a38325f
zerghua <zerghua@gmail.com>
2016-08-26 21:32:50 -0400

    378, minHeap. or can convert matrix to sorted list and get the first k.

---
+++ leetcode/src/leetcode/N378_KthSmallestElementinaSortedMatrix.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Created by Hua on 8/26/16.
+
+ Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+ find the kth smallest element in the matrix.
+
+ Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+ Example:
+
+ matrix = [
+ [ 1,  5,  9],
+ [10, 11, 13],
+ [12, 13, 15]
+ ],
+ k = 8,
+
+ return 13.
+
+ Note:
+ You may assume k is always valid, 1 ≤ k ≤ n2.
+
+
+
+ */
+public class N378_KthSmallestElementinaSortedMatrix {
+    public class Pair implements Comparable<Pair>{
+        int x, y,val;
+        public Pair(int x, int y, int val){
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+
+        @Override
+        public int compareTo(Pair o) {
+            return this.val - o.val;
+        }
+    }
+
+    // 36 ms heap
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>();
+        for(int i=0;i<n;i++) minHeap.add(new Pair(0,i, matrix[0][i])); // add first row
+
+        for(int i=0;i<k-1;i++){
+            Pair p = minHeap.poll();
+            if(p.x == n-1) continue;
+            minHeap.add(new Pair(p.x+1, p.y, matrix[p.x+1][p.y]));
+        }
+        return minHeap.poll().val;
+    }
+}
