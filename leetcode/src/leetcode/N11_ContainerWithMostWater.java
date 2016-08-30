60bc13bc9b52c2962c5c73a99bbbee611950ed8f
zerghua <zerghua@gmail.com>
2016-08-30 11:01:43 -0400

    11, container with most water. two pointers and greedy. added bug free code.

--- leetcode/src/leetcode/N11_ContainerWithMostWater.java
+++ leetcode/src/leetcode/N11_ContainerWithMostWater.java
@@ -2,6 +2,12 @@ package leetcode;
 
 /**
  * Created by Hua on 3/18/2016.
+ *
+ * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+
+ Note: You may not slant the container.
  */
 public class N11_ContainerWithMostWater {
     //exceed time limit
@@ -15,6 +21,7 @@ public class N11_ContainerWithMostWater {
         return max;
     }
 
+    // this might have a bug when left and right height are the same.
     public int maxArea2(int[] height) {
         int i=0, j=height.length-1;
         int min_height = 0;
@@ -32,4 +39,22 @@ public class N11_ContainerWithMostWater {
 
         return max;
     }
+
+    // should be bug free
+    // greedy, can also be done in DP, but need extra space.
+    // 4 ms
+    public int maxArea3(int[] height) {
+        int left = 0, right = height.length-1;
+        int max = 0;
+        while(left < right){
+            int min = Math.min(height[right] , height[left]);
+            max = Math.max(max,  min * (right-left));
+
+            while(left<right && height[left]<= min) left++;
+            while(left<right && height[right]<= min) right--;
+        }
+        return max;
+    }
+
+
 }
