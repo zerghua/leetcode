ed5e4048aea0edda1c2dd0400bc1b86081cad990
zerghua <zerghua@gmail.com>
2016-08-30 13:31:42 -0400

    16, add problem description.

--- leetcode/src/leetcode/N16_3SumClosest.java
+++ leetcode/src/leetcode/N16_3SumClosest.java
@@ -1,9 +1,17 @@
 package leetcode;
-
 import java.util.Arrays;
 
 /**
  * Created by Hua on 3/27/2016.
+
+ Given an array S of n integers, find three integers in S such that the sum is closest to a given number,target.
+ Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+ For example, given array S = {-1 2 1 -4}, and target = 1.
+
+ The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+
  */
 public class N16_3SumClosest {
     // o(n^2) time,
