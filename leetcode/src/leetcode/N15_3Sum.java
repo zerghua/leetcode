306d46af17afb8f146bbb2e9a045e1d3722b8904
zerghua <zerghua@gmail.com>
2016-08-30 13:02:44 -0400

    15, add problem description.

--- leetcode/src/leetcode/N15_3Sum.java
+++ leetcode/src/leetcode/N15_3Sum.java
@@ -2,6 +2,21 @@ package leetcode;
 import java.util.*;
 /**
  * Created by Hua on 4/9/2016.
+ *
+ Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+ Find all unique triplets in the array which gives the sum of zero.
+
+ Note: The solution set must not contain duplicate triplets.
+
+ For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+ A solution set is:
+ [
+     [-1, 0, 1],
+     [-1, -1, 2]
+ ]
+
+ *
  */
 public class N15_3Sum {
     //8 ms  O(n^2) time
