3553b78214a3b62d0f7a6d73fdf64c99632cb80d
zerghua <zerghua@gmail.com>
2016-08-30 11:40:00 -0400

    14, vertical scanning of each string. o(s), s is sum of all the strings

--- leetcode/src/leetcode/N14_LongestCommonPrefix.java
+++ leetcode/src/leetcode/N14_LongestCommonPrefix.java
@@ -1,7 +1,14 @@
 package leetcode;
 
+/*
+	Write a function to find the longest common prefix string amongst an array of strings.
+
+ */
+
+
 public class N14_LongestCommonPrefix {
-	//2 ms
+	// 2 ms
+	// vertical scanning of each string. o(s), s is sum of all the strings
     public String longestCommonPrefix(String[] strs) {
     	if(strs == null || strs.length == 0) return "";
         int min_size=Integer.MAX_VALUE;
