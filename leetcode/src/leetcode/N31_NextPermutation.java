c416be11ce4b2038a743f6486d389fde624f30b3
Hua <zerghua@gmail.com>
2016-09-02 11:10:11 -0400

    31, added more comment, but still need to find out the rule of switch. that's the hard part.

--- leetcode/src/leetcode/N31_NextPermutation.java
+++ leetcode/src/leetcode/N31_NextPermutation.java
@@ -2,10 +2,47 @@ package leetcode;
 
 /**
  * Created by Hua on 4/6/2016.
+
+ Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+
+ If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+ The replacement must be in-place, do not allocate extra memory.
+
+ Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ 1,2,3 → 1,3,2
+ 3,2,1 → 1,2,3
+ 1,1,5 → 1,5,1
+
+
+ the hard part is to understand what is "lexicographically next greater permutation of numbers"
+ 1,2,3,4,5,6
+ 1,2,3,4,6,5
+ 1,2,3,5,4,6
+ 1,2,3,5,6,4
+ 1,2,3,6,4,5
+ 1,2,3,6,5,4
+ 1,2,4,3,5,6
+
+
+
+
+ 4,5,6,3,2,1   ->  4,6,1,2,3,5
+   | |
+   p q
+
+
+
+ break down of above:
+
+ 1.  from right to left, find the first element that is less than its previous one.
+
+
+
  */
 
 // http://www.programcreek.com/2014/06/leetcode-next-permutation-java/
-    // 2 ms
+// 2 ms
 public class N31_NextPermutation {
     public void nextPermutation(int[] nums) {
         int change_index=0, next_index=0;
