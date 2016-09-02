0541e37d76214498c2f8e3a12bb076d09d744126
Hua <zerghua@gmail.com>
2016-09-02 11:21:16 -0400

    34, binary search for a range. two recursive call inside equal condition.

--- leetcode/src/leetcode/N34_SearchforaRange.java
+++ leetcode/src/leetcode/N34_SearchforaRange.java
@@ -2,6 +2,20 @@ package leetcode;
 
 /**
  * Created by Hua on 3/27/2016.
+
+ Given a sorted array of integers, find the starting and ending position of a given target value.
+
+ Your algorithm's runtime complexity must be in the order of O(log n).
+
+ If the target is not found in the array, return [-1, -1].
+
+ For example,
+ Given [5, 7, 7, 8, 8, 10] and target value 8,
+ return [3, 4].
+
+
+ I like this problem and solution, two recursive call inside equal condition.
+
  */
 public class N34_SearchforaRange {
     public void binarySearch(int[] nums, int target, int start, int end, int[] ret){
