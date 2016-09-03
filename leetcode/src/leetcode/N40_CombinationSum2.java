f4cde553e175029b410920f181993564077143cd
zerghua <zerghua@gmail.com>
2016-09-03 10:20:04 -0400

    40, add problem description.

--- leetcode/src/leetcode/N40_CombinationSum2.java
+++ leetcode/src/leetcode/N40_CombinationSum2.java
@@ -6,6 +6,28 @@ import java.util.List;
 
 /**
  * Created by Hua on 3/25/2016.
+
+ Given a collection of candidate numbers (C) and a target number (T),
+ find all unique combinations in C where the candidate numbers sums to T.
+
+ Each number in C may only be used once in the combination.
+
+ Note:
+
+ All numbers (including target) will be positive integers.
+ The solution set must not contain duplicate combinations.
+
+ For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8,
+ A solution set is:
+
+ [
+     [1, 7],
+     [1, 2, 5],
+     [2, 6],
+     [1, 1, 6]
+ ]
+
+
  */
 public class N40_CombinationSum2 {
     public void combinationSum_helper_better(int[] candidates, int start, int target,
