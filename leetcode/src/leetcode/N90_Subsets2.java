421c9ac5afae6ef3712a0d831670ab3642c28f45
zerghua <zerghua@gmail.com>
2016-09-06 11:41:00 -0400

    90, subset 2. DFS + backtracking. skip duplicate when i>start && nums[i-1] == nums[i]

--- leetcode/src/leetcode/N90_Subsets2.java
+++ leetcode/src/leetcode/N90_Subsets2.java
@@ -1,14 +1,31 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Hua on 3/24/2016.
+
+ Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+
+ Note: The solution set must not contain duplicate subsets.
+
+ For example,
+ If nums = [1,2,2], a solution is:
+
+ [
+     [2],
+     [1],
+     [1,2,2],
+     [2,2],
+     [1,2],
+     []
+ ]
+
+
  */
 public class N90_Subsets2 {
+
+    // version 1
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ret = new LinkedList<List<Integer>>();
         HashSet<List<Integer>> hs = new HashSet<>();
@@ -64,7 +81,7 @@ public class N90_Subsets2 {
         }
     }
 
-
+    // version 2
     public List<List<Integer>> subsets2_backtracking(int[] nums) {
         List<List<Integer>> ret = new LinkedList<List<Integer>>();
         LinkedList<Integer> list = new LinkedList<>();
@@ -75,4 +92,32 @@ public class N90_Subsets2 {
         subsets2_backtracking_helper(nums, 0, list, ret);
         return ret;
     }
+
+
+    // version 3 adde on 9/6/2016
+    // 4 ms  19 / 19 test cases passed.
+    // DFS + backtracking. skip duplicate when i>start && nums[i-1] == nums[i]
+    public class Solution {
+        public void dfs(List<List<Integer>> ret, int[] nums, List<Integer> cur_list, int start){
+            for(int i=start;i<nums.length;i++){
+                if(i>start && nums[i] == nums[i-1])continue; // to skip duplicate
+                cur_list.add(nums[i]);
+                ret.add(new ArrayList<>(cur_list));
+                dfs(ret, nums, cur_list, i+1);
+                cur_list.remove(cur_list.size()-1);
+            }
+        }
+
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            List<Integer> cur_list = new ArrayList<>();
+            List<List<Integer>> ret = new ArrayList<>();
+            ret.add(cur_list);
+            if (nums == null || nums.length == 0) return ret;
+            Arrays.sort(nums);
+            dfs(ret, nums, cur_list, 0);
+            return ret;
+        }
+    }
+
+
 }
