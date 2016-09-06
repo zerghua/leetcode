451a0d4f3221e93090c1c562af25ddb140da17b9
zerghua <zerghua@gmail.com>
2016-09-06 13:07:44 -0400

    77, add problem description.

--- leetcode/src/leetcode/N77_Combinations.java
+++ leetcode/src/leetcode/N77_Combinations.java
@@ -6,6 +6,22 @@ import java.util.List;
 
 /**
  * Created by Hua on 3/20/2016.
+
+ Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+ For example,
+ If n = 4 and k = 2, a solution is:
+
+ [
+     [2,4],
+     [3,4],
+     [2,3],
+     [1,2],
+     [1,3],
+     [1,4],
+ ]
+
+
  */
 public class N77_Combinations {
 
@@ -37,5 +53,4 @@ public class N77_Combinations {
 
         return ret;
     }
-
 }
