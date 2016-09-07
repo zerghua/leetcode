ce3a0c2556a8f096ac366fa4aeb65510dd89929f
zerghua <zerghua@gmail.com>
2016-09-07 14:41:29 -0400

    54, print spiral matrix. array operation.  4 loops inside one loop. 4 sides(top, bottom, left and right). ++ or --

--- leetcode/src/leetcode/N54_SpiralMatrix.java
+++ leetcode/src/leetcode/N54_SpiralMatrix.java
@@ -59,4 +59,42 @@ public class N54_SpiralMatrix {
         // recur
         spiralOrder(matrix, ret, left, right, top, bottom);
     }
+
+    // version 2 added on 9/7/2016
+    // 4 sides(top, bottom, left and right). ++ or --
+    // 1 ms  22 / 22 test cases passed.
+    public class Solution {
+        public List<Integer> spiralOrder(int[][] matrix) {
+            List<Integer> ret = new ArrayList<>();
+            if (matrix == null || matrix.length==0) return ret;
+            int top = 0, left = 0, bottom = matrix.length-1, right = matrix[0].length-1;
+            while(left<= right || top <= bottom){
+                // top side
+                if(top <= bottom){
+                    for(int i=left; i<=right;i++) ret.add(matrix[top][i]);
+                    top++;
+                }
+
+                // right side
+                if(left <= right){
+                    for(int i=top;i<=bottom;i++) ret.add(matrix[i][right]);
+                    right--;
+                }
+
+                // bottom side
+                if(top <= bottom){
+                    for(int i=right; i>=left;i--) ret.add(matrix[bottom][i]);
+                    bottom--;
+                }
+
+                // right side
+                if(left <= right){
+                    for(int i=bottom;i>=top;i--) ret.add(matrix[i][left]);
+                    left++;
+                }
+            }
+
+            return ret;
+        }
+    }
 }
