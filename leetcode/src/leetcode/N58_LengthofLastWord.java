7f2fb6472e77b20ab9e1e8a160bd4c99980a65bb
zerghua <zerghua@gmail.com>
2016-09-07 15:51:44 -0400

    58, length of last word. String operation. scan from last. o(n)

--- leetcode/src/leetcode/N58_LengthofLastWord.java
+++ leetcode/src/leetcode/N58_LengthofLastWord.java
@@ -1,5 +1,17 @@
 package leetcode;
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+return the length of last word in the string.
 
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example,
+Given s = "Hello World",
+return 5.
+
+ */
 public class N58_LengthofLastWord {
 	//0 ms
     public int lengthOfLastWord(String s) {
@@ -15,4 +27,22 @@ public class N58_LengthofLastWord {
         }
         return len;
     }
+
+    // version 2 added on 9/7/2016
+    // 0 ms  59 / 59 test cases passed.
+    // String operation. scan from last. o(n)
+    public class Solution {
+        public int lengthOfLastWord(String s) {
+            if(s.length() ==0 || s==null) return 0;
+            int ret=0;
+            for(int i=s.length()-1; i>=0;i--){
+                if(s.charAt(i) != ' ') ret++;
+                else {
+                    if(ret != 0) break;  // trailing spaces.
+                }
+            }
+            return ret;
+        }
+    }
+
 }
