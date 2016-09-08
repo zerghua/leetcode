40f764261769fcb0678f96412f93883126967bdd
zerghua <zerghua@gmail.com>
2016-09-08 11:04:19 -0400

    125, valid palindrome. add problem description.

--- leetcode/src/leetcode/N125_ValidPalindrome.java
+++ leetcode/src/leetcode/N125_ValidPalindrome.java
@@ -20,6 +20,20 @@ package leetcode;
  
 		System.out.println(isValidPalindrome(str));
 	}
+
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+
+
  */
 public class N125_ValidPalindrome {
 	//7 ms
