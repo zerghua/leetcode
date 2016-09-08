d943171b4c0a856668dfb90e1fc718a326938bf3
zerghua <zerghua@gmail.com>
2016-09-07 20:39:40 -0400

    60, permutation sequence. math, factorial and find math pattern.

--- leetcode/src/leetcode/N60_PermutationSequence.java
+++ leetcode/src/leetcode/N60_PermutationSequence.java
@@ -2,6 +2,24 @@ package leetcode;
 import java.util.*;
 /**
  * Created by Hua on 4/8/2016.
+
+ The set [1,2,3,…,n] contains a total of n! unique permutations.
+
+ By listing and labeling all of the permutations in order,
+ We get the following sequence (ie, for n = 3):
+
+ "123"
+ "132"
+ "213"
+ "231"
+ "312"
+ "321"
+
+ Given n and k, return the kth permutation sequence.
+
+ Note: Given n will be between 1 and 9 inclusive.
+
+ https://discuss.leetcode.com/topic/17348/explain-like-i-m-five-java-solution-in-o-n
  */
 public class N60_PermutationSequence {
     //4 ms, had to use hard-coded factorial, or will TLE
