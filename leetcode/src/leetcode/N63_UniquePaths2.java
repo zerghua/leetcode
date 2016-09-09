181305383dc462c8cecb3723cf123163fa72f996
zerghua <zerghua@gmail.com>
2016-09-09 10:49:30 -0400

    63, easy-medium DP. rolling array.   dp[j] += dp[j-1]

--- leetcode/src/leetcode/N63_UniquePaths2.java
+++ leetcode/src/leetcode/N63_UniquePaths2.java
@@ -2,6 +2,27 @@ package leetcode;
 
 /**
  * Created by Hua on 3/26/2016.
+
+ Follow up for "Unique Paths":
+
+ Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+ An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+ For example,
+
+ There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+ [
+ [0,0,0],
+ [0,1,0],
+ [0,0,0]
+ ]
+
+ The total number of unique paths is 2.
+
+ Note: m and n will be at most 100.
+
  */
 public class N63_UniquePaths2 {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
@@ -34,4 +55,28 @@ public class N63_UniquePaths2 {
 
         return ret[rows-1][cols-1];
     }
+
+    // version 2 added on 9/9/2016
+    // medium, 1-d DP.
+    // 1ms 43 / 43 test cases passed.
+    public class Solution {
+        public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+            int row = obstacleGrid.length;
+            int col = obstacleGrid[0].length;
+            int[] dp = new int[col];
+            if(obstacleGrid == null || row==0 || obstacleGrid[0][0]==1)
+                return 0;
+
+            dp[0] = 1;
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col;j++){
+                    if(obstacleGrid[i][j] == 1) dp[j] = 0;
+                    else if(j>0) dp[j] += dp[j-1];
+                }
+            }
+            return dp[col-1];
+        }
+    }
+
+
 }
