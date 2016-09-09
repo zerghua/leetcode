bf98cbc6c86959719e280a149b1bdc61c7e801d8
zerghua <zerghua@gmail.com>
2016-09-09 10:28:38 -0400

    62, unique path. Easy DP.  dp[i][j] = dp[i-1][j] + dp[i][j-1]

--- leetcode/src/leetcode/N62_UniquePaths.java
+++ leetcode/src/leetcode/N62_UniquePaths.java
@@ -36,4 +36,22 @@ public class N62_UniquePaths {
         if(n==1) return 1;
         return uniquePaths(m-1, n) +  uniquePaths(m, n-1);
     }
+
+    // version 2 added on 9/9/2016
+    // 2-d DP. Easy.  dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    // 1 ms  61 / 61 test cases passed.
+    public class Solution {
+        public int uniquePaths(int m, int n) {
+            if(m<=0 || n<=0) return 0;
+            int[][] dp = new int[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(i==0 || j==0) dp[i][j] = 1;
+                    else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+            return dp[m-1][n-1];
+        }
+    }
+
 }
