44ab62fa7ed78a408ec7082f915e8b60c7bbe829
zerghua <zerghua@gmail.com>
2016-09-09 18:05:30 -0400

    74,    binary search in 2-d matrix, val=matrix[mid/col][mid%col],  this solution is guaranteed by problem description

--- leetcode/src/leetcode/N74_Searcha2DMatrix.java
+++ leetcode/src/leetcode/N74_Searcha2DMatrix.java
@@ -2,6 +2,25 @@ package leetcode;
 
 /**
  * Created by Hua on 3/21/2016.
+
+ Write an efficient algorithm that searches for a value in an m x n matrix.
+ This matrix has the following properties:
+
+ Integers in each row are sorted from left to right.
+ The first integer of each row is greater than the last integer of the previous row.
+
+ For example,
+
+ Consider the following matrix:
+
+ [
+     [1,   3,  5,  7],
+     [10, 11, 16, 20],
+     [23, 30, 34, 50]
+ ]
+
+ Given target = 3, return true.
+
  */
 
 
@@ -33,5 +52,24 @@ public class N74_Searcha2DMatrix {
         }
 
         return false;
+    }
+
+    // version 2 added on 9/9/2016
+    // binary search in 2-d matrix, val=matrix[mid/col][mid%col]
+    // this solution is guaranteed by problem description
+    // 0 ms  134 / 134 test cases passed.
+    public class Solution {
+        public boolean searchMatrix(int[][] matrix, int target) {
+            int row = matrix.length, col = matrix[0].length;
+            int left = 0, right= row * col - 1;
+            while(left <= right){
+                int mid = (right-left)/2 + left;
+                int midValue = matrix[mid/col][mid%col];
+                if (midValue == target) return true;
+                else if (midValue >target) right = mid -1;
+                else left = mid + 1;
+            }
+            return false;
+        }
     }
 }
