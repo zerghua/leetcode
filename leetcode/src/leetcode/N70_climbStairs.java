68a8f70728f015e7b1e1c04f94427d134f7a28ca
zerghua <zerghua@gmail.com>
2016-09-09 16:58:25 -0400

    70, climb stairs. either 1 step or 2 steps. dp[i] = dp[i-2] + dp[i-1].  or c= a+b, a=b, b=c

--- leetcode/src/leetcode/N70_climbStairs.java
+++ leetcode/src/leetcode/N70_climbStairs.java
@@ -1,5 +1,10 @@
 package leetcode;
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
 
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ */
 //fibonacci sequence
 public class N70_climbStairs {
 	
@@ -25,6 +30,19 @@ public class N70_climbStairs {
 			b= c;
     	}
     	return b;
+    }
+
+    // version 3 added on 9/9/2016
+    // DP, dp[i] = dp[i-2] + dp[i-1]
+    // 0 ms  45 / 45 test cases passed.
+    public class Solution {
+        public int climbStairs(int n) {
+            if(n<0) return 0;
+            int[] dp = new int[n+1];
+            dp[0] = dp[1] = 1;
+            for(int i=2;i<=n;i++) dp[i] = dp[i-2] + dp[i-1];
+            return dp[n];
+        }
     }
         
     
