f5c8ebb2db2f26d3b5c4a83080aa3663d898abd1
zerghua <zerghua@gmail.com>
2016-09-10 11:43:00 -0400

    80, similar to 26, rather than compare with i, this time compares with i-1. if equal, continue to skip it, else num[++i] = nums[j]

--- leetcode/src/leetcode/N80_RemoveDuplicatesfromSortedArray2.java
+++ leetcode/src/leetcode/N80_RemoveDuplicatesfromSortedArray2.java
@@ -2,6 +2,15 @@ package leetcode;
 
 /**
  * Created by Hua on 3/21/2016.
+
+ Follow up for "Remove Duplicates":
+ What if duplicates are allowed at most twice?
+
+ For example,
+ Given sorted array nums = [1,1,1,2,2,3],
+
+ Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+
  */
 public class N80_RemoveDuplicatesfromSortedArray2 {
 
@@ -39,4 +48,20 @@ public class N80_RemoveDuplicatesfromSortedArray2 {
         }
         return end+1;
     }
+
+    // version 3 added on 9/10/2016
+    // 1 ms  164 / 164 test cases passed.
+    // similar to 26, rather than compare with i, this time compares with i-1.
+    // if equal, continue to skip it, else num[++i] = nums[j]
+    public class Solution {
+        public int removeDuplicates(int[] nums) {
+            if(nums.length <3) return nums.length;
+            int i=1;
+            for(int j=2;j<nums.length;j++){
+                if(nums[i-1] != nums[j]) nums[++i] = nums[j];
+            }
+            return i+1;
+        }
+    }
+
 }
