3cb3ba2d396959142aaedb46a6523003633035fc
zerghua <zerghua@gmail.com>
2016-09-10 10:59:49 -0400

    79, search a word in matrix. 4 DFS+backtracking, use visited[][] as backtrack.

--- leetcode/src/leetcode/N79_WordSearch.java
+++ leetcode/src/leetcode/N79_WordSearch.java
@@ -2,6 +2,24 @@ package leetcode;
 
 /**
  * Created by Hua on 4/10/2016.
+
+ Given a 2D board and a word, find if the word exists in the grid.
+
+ The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ For example,
+ Given board =
+
+ [
+     ['A','B','C','E'],
+     ['S','F','C','S'],
+     ['A','D','E','E']
+ ]
+
+ word = "ABCCED", -> returns true,
+ word = "SEE", -> returns true,
+ word = "ABCB", -> returns false.
+
  */
 public class N79_WordSearch {
     //16 ms
