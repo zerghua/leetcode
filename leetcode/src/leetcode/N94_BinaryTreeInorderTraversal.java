ddfb8750f8d66fc86fbb81d415799accdd942d30
zerghua <zerghua@gmail.com>
2016-09-11 16:26:28 -0400

    94, add problem description.

--- leetcode/src/leetcode/N94_BinaryTreeInorderTraversal.java
+++ leetcode/src/leetcode/N94_BinaryTreeInorderTraversal.java
@@ -1,7 +1,23 @@
 package leetcode;
 import java.util.*;
 import leetcode.N0_data_strcture.*;
+/*
+Given a binary tree, return the inorder traversal of its nodes' values.
 
+For example:
+Given binary tree [1,null,2,3],
+
+   1
+    \
+     2
+    /
+   3
+
+return [1,3,2].
+
+Note: Recursive solution is trivial, could you do it iteratively?
+
+ */
 public class N94_BinaryTreeInorderTraversal {
 	
 	// iterative non extra space  2 ms
