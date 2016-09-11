2c50ac8a61584f75523f660cfcf45c8eecc3b897
zerghua <zerghua@gmail.com>
2016-09-11 17:31:46 -0400

    96, unique binary search tree. DP catalan number, f(n) = f(0)*f(n-1) + f(1)*f(n-2) +... +f(n-1)f(0)

--- leetcode/src/leetcode/N96_UniqueBinarySearchTrees.java
+++ leetcode/src/leetcode/N96_UniqueBinarySearchTrees.java
@@ -1,18 +1,19 @@
 package leetcode;
 /*
- * https://en.wikipedia.org/wiki/Catalan_number
- 定义f(n)为unique BST的数量，以n = 3为例：
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 
-构造的BST的根节点可以取{1, 2, 3}中的任一数字。
+For example,
+Given n = 3, there are a total of 5 unique BST's.
 
-如以1为节点，则left subtree只能有0个节点，而right subtree有2, 3两个节点。
-所以left/right subtree一共的combination数量为：f(0) * f(2) = 2
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
 
-以2为节点，则left subtree只能为1，right subtree只能为2：f(1) * f(1) = 1
 
-以3为节点，则left subtree有1, 2两个节点，right subtree有0个节点：f(2)*f(0) = 2
+https://en.wikipedia.org/wiki/Catalan_number
 
-总结规律：
 f(0) = 1
 f(n) = f(0)*f(n-1) + f(1)*f(n-2) + ... + f(n-2)*f(1) + f(n-1)*f(0)
 
@@ -31,9 +32,9 @@ i=4, count[4]=count[0]*count[3] // 1 is root
  */
 
 
-
 public class N96_UniqueBinarySearchTrees {
 	//0 ms
+    // F(i, n) = G(i) * G(n-i-1)	0 <= i < n
     public int numTrees(int n) {
         int[] catalan = new int[n+1];
         catalan[0]=1;
