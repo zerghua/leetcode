21633c7e3853b7af555838b173019bb055e472fe
zerghua <zerghua@gmail.com>
2016-09-10 20:23:43 -0400

    83, add problem description and some comment.

--- leetcode/src/leetcode/N83_deleteDuplicates.java
+++ leetcode/src/leetcode/N83_deleteDuplicates.java
@@ -1,9 +1,18 @@
 package leetcode;
-
 import leetcode.N0_data_strcture.*;
+
+/*
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+
+ */
 
 public class N83_deleteDuplicates {
 	// 316 ms  83%
+    // linked list skip next one if equals.
     public ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
         while(cur != null && cur.next!=null){
