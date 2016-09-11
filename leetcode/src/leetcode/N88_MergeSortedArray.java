0e4dce25a8c365060f563cd8ee19d7cdffb1c5b3
zerghua <zerghua@gmail.com>
2016-09-11 12:43:46 -0400

    88, merge sorted array. fill from the end of array.

--- leetcode/src/leetcode/N88_MergeSortedArray.java
+++ leetcode/src/leetcode/N88_MergeSortedArray.java
@@ -1,4 +1,13 @@
 package leetcode;
+
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+
+ */
+
 public class N88_MergeSortedArray {
 	//0 ms
     public void merge(int[] nums1, int m, int[] nums2, int n) {
@@ -25,6 +34,7 @@ public class N88_MergeSortedArray {
     }
     
     // 0 ms
+    // fill from the end of array.
     public void merge2(int[] nums1, int m, int[] nums2, int n) {
         while(m>0 && n>0){
         	if(nums1[m-1] > nums2[n-1]) {
