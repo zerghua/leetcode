3066a355014d77224d8ddad74155e1a29a503961
zerghua <zerghua@gmail.com>
2016-09-11 14:36:08 -0400

    91, decode ways. DP, f(n) = f(n-1) + f(n-2) if substring(n-1,n) and substring(n-2,n) are valid.

--- leetcode/src/leetcode/N91_DecodeWays.java
+++ leetcode/src/leetcode/N91_DecodeWays.java
@@ -3,6 +3,21 @@ import java.util.*;
 
 /**
  * Created by Hua on 4/16/2016.
+
+ A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+ 'A' -> 1
+ 'B' -> 2
+ ...
+ 'Z' -> 26
+
+ Given an encoded message containing digits, determine the total number of ways to decode it.
+
+ For example,
+ Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+ The number of ways decoding "12" is 2.
+
  */
 public class N91_DecodeWays {
     //TLE, but it saved all the decode ways
