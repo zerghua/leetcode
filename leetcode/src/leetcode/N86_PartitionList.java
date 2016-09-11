c24e0bd889d622d6b02fff72a52759d6c4643ca3
zerghua <zerghua@gmail.com>
2016-09-11 12:03:25 -0400

    86,   two dummy node and two pointers, dummy node points to the start of list, p is the current pointer, will ended up point to the end of list.

--- leetcode/src/leetcode/N86_PartitionList.java
+++ leetcode/src/leetcode/N86_PartitionList.java
@@ -3,6 +3,15 @@ import leetcode.N0_data_strcture.*;
 
 /**
  * Created by Hua on 3/26/2016.
+ *
+ Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+ You should preserve the original relative order of the nodes in each of the two partitions.
+
+ For example,
+ Given 1->4->3->2->5->2 and x = 3,
+ return 1->2->2->4->3->5.
+
  */
 public class N86_PartitionList {
 
@@ -27,5 +36,31 @@ public class N86_PartitionList {
         }
 
         return dummy.next;
+    }
+
+
+    // version 2 added on 9/11/2016
+    // two dummy node and two pointers, dummy node points to the start of list, p is the current
+    // pointer, will ended up point to the end of list.
+    // 1 ms  166 / 166 test cases passed.
+    public class Solution {
+        public ListNode partition(ListNode head, int x) {
+            ListNode dummy1 = new ListNode(0);
+            ListNode dummy2 = new ListNode(0);
+            ListNode p1 = dummy1, p2 = dummy2;
+            while(head != null) {
+                if(head.val < x){
+                    p1.next = head;
+                    p1 = p1.next;
+                }else{
+                    p2.next = head;
+                    p2 = p2.next;
+                }
+                head = head.next;
+            }
+            p1.next = dummy2.next;
+            p2.next = null;
+            return dummy1.next;
+        }
     }
 }
