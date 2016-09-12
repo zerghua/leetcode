0b5370b31746e1f789038470cfc32701ebc77142
zerghua <zerghua@gmail.com>
2016-09-12 15:05:12 -0400

    104, max depth of binary tree. DFS. return 1+ max(dfs(left), dfs(right))

--- leetcode/src/leetcode/N104_maxDepth.java
+++ leetcode/src/leetcode/N104_maxDepth.java
@@ -1,12 +1,16 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ */
 
 // 316 ms  87%
 public class N104_maxDepth {
     public int maxDepth(TreeNode root) {
-        if (root == null)
-        	return 0;
-        
+        if (root == null) return 0;
         return 1+ Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 }
