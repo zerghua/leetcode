8f794d04621f1de83d1277e643a60b01bac3386d
zerghua <zerghua@gmail.com>
2016-09-12 10:33:32 -0400

    101, symmetric tree. DFS, compare left.left with right.right and left.right and right.left. iteratively, use two queue, goes into left queue as left.push(left,right), right.push(right,left).

--- leetcode/src/leetcode/N101_SymmetricTree.java
+++ leetcode/src/leetcode/N101_SymmetricTree.java
@@ -2,7 +2,31 @@ package leetcode;
 import java.util.LinkedList;
 import java.util.Queue;
 import leetcode.N0_data_strcture.*;
+/*
 
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+But the following [1,2,2,null,3,null,3] is not:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3
+
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+
+
+ */
 public class N101_SymmetricTree {
 	//340 ms  37%
 	public boolean isSymmetricHelper(TreeNode l, TreeNode r){
