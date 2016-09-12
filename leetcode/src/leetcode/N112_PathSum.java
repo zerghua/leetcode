34f98be6a084fd5ac4bc7586c429accda181b11a
zerghua <zerghua@gmail.com>
2016-09-12 19:40:57 -0400

    112, path sum. DFS.

--- leetcode/src/leetcode/N112_PathSum.java
+++ leetcode/src/leetcode/N112_PathSum.java
@@ -1,6 +1,22 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+/*
 
+ Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+For example:
+Given the below binary tree and sum = 22,
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+ */
 public class N112_PathSum {
 	//1 ms
     public boolean hasPathSum(TreeNode root, int sum) {
