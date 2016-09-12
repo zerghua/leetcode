317d5a936f584d85dc55687e84e451b1f12a746b
zerghua <zerghua@gmail.com>
2016-09-12 19:51:38 -0400

    113, path sum2. DFS+ backtracking.

--- leetcode/src/leetcode/N113_PathSum2.java
+++ leetcode/src/leetcode/N113_PathSum2.java
@@ -4,6 +4,26 @@ import java.util.*;
 
 /**
  * Created by Hua on 4/1/2016.
+
+ Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ For example:
+ Given the below binary tree and sum = 22,
+
+       5
+      / \
+     4   8
+    /   / \
+   11  13  4
+  /  \    / \
+ 7    2  5   1
+
+ return
+
+ [
+     [5,4,11,2],
+     [5,8,4,5]
+ ]
+
  */
 public class N113_PathSum2 {
     //31 ms  -> 4 ms
