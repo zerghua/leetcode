c2dfa00c45416c80c18f3f5866c0004267736c47
zerghua <zerghua@gmail.com>
2016-09-11 20:05:42 -0400

    98, add problem description.

--- leetcode/src/leetcode/N98_ValidateBinarySearchTree.java
+++ leetcode/src/leetcode/N98_ValidateBinarySearchTree.java
@@ -2,7 +2,32 @@ package leetcode;
 import leetcode.N0_data_strcture.*;
 
 /**
- * Created by zhanhua on 4/14/2016.
+ * Created by Hua on 4/14/2016.
+
+ Given a binary tree, determine if it is a valid binary search tree (BST).
+
+ Assume a BST is defined as follows:
+
+ The left subtree of a node contains only nodes with keys less than the node's key.
+ The right subtree of a node contains only nodes with keys greater than the node's key.
+ Both the left and right subtrees must also be binary search trees.
+
+ Example 1:
+
+ 2
+ / \
+ 1   3
+
+ Binary tree [2,1,3], return true.
+
+ Example 2:
+
+ 1
+ / \
+ 2   3
+
+ Binary tree [1,2,3], return false.
+
  */
 public class N98_ValidateBinarySearchTree {
     //1 ms
