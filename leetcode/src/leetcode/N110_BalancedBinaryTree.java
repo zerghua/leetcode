20e11d803a8beaaca7f79329f616187115e60def
zerghua <zerghua@gmail.com>
2016-09-12 18:57:06 -0400

    110, check if binary tree is balanced.  get height on left and right children and check.

--- leetcode/src/leetcode/N110_BalancedBinaryTree.java
+++ leetcode/src/leetcode/N110_BalancedBinaryTree.java
@@ -1,6 +1,12 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+/*
 
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+ */
 public class N110_BalancedBinaryTree {
 	/*  this is special case only compares depth difference from root, should consider every node
 	public int maxDepth(TreeNode root){
