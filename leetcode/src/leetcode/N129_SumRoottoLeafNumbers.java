a760848160f29dcd14fc872ae523e35bf5ccaa15
zerghua <zerghua@gmail.com>
2016-09-13 19:43:42 -0400

    129, sum of each path from root to leaf. DFS, return accumulated sum

--- leetcode/src/leetcode/N129_SumRoottoLeafNumbers.java
+++ leetcode/src/leetcode/N129_SumRoottoLeafNumbers.java
@@ -5,6 +5,25 @@ import java.util.LinkedList;
 
 /**
  * Created by Hua on 3/21/2016.
+
+ Given a binary tree containing digits from 0-9 only,
+ each root-to-leaf path could represent a number.
+
+ An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+ Find the total sum of all root-to-leaf numbers.
+
+ For example,
+
+   1
+  / \
+ 2   3
+
+ The root-to-leaf path 1->2 represents the number 12.
+ The root-to-leaf path 1->3 represents the number 13.
+
+ Return the sum = 12 + 13 = 25.
+
  */
 public class N129_SumRoottoLeafNumbers {
     public int sumNumbers(TreeNode root) {
@@ -58,4 +77,19 @@ public class N129_SumRoottoLeafNumbers {
         return sumNumbers_recursion_helper(root, 0, 0);
     }
 
+    // version 3  added on 9/13/2016
+    // 0 ms, 109 / 109 test cases passed.
+    // DFS, return accumulated sum
+    public class Solution {
+        public int sumNumbers(TreeNode root) {
+            return dfs(root, 0);
+        }
+
+        public int dfs(TreeNode node, int sum){
+            if(node == null) return 0;
+            int ret = sum*10 + node.val;
+            if(node.left == null && node.right==null) return ret;
+            return dfs(node.left, ret) + dfs(node.right, ret);
+        }
+    }
 }
