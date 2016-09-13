4bc3b8c5585ae93792793fd18e0add69be059853
zerghua <zerghua@gmail.com>
2016-09-13 11:20:47 -0400

    118, pascal triangle. array, find pattern.

--- leetcode/src/leetcode/N118_PascalsTriangle.java
+++ leetcode/src/leetcode/N118_PascalsTriangle.java
@@ -1,5 +1,21 @@
 package leetcode;
 import java.util.*;
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+
+
+ */
 
 public class N118_PascalsTriangle {
 	//2 ms
@@ -49,5 +65,27 @@ public class N118_PascalsTriangle {
     		pre = cur;
     	}
     	return ret;
+    }
+
+
+    // version 3 added on 9/13/2016
+    // 1 ms 15 / 15 test cases passed.
+    public class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> ret = new ArrayList<>();
+            if(numRows <= 0 ) return ret;
+            ret.add(new ArrayList<>(Arrays.asList(1)));
+            while(numRows-- > 1){
+                ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1));
+                List<Integer> pre_list = ret.get(ret.size()-1);
+                int size =pre_list.size();
+                for(int i=0;i<size-1;i++){
+                    list.add(pre_list.get(i)+ pre_list.get(i+1));
+                }
+                list.add(1);
+                ret.add(list);
+            }
+            return ret;
+        }
     }
 }
