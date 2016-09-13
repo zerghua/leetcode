283b59e680664e521324a888da624d7dc868b7c3
zerghua <zerghua@gmail.com>
2016-09-13 11:23:40 -0400

    119, pascal triangle 2. only allow o(k) space, rolling array, scan from backward

--- leetcode/src/leetcode/N119_PascalsTriangle2.java
+++ leetcode/src/leetcode/N119_PascalsTriangle2.java
@@ -1,16 +1,18 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
-[
-     [1],
-    [1,1],
-   [1,2,1],
-  [1,3,3,1],
- [1,4,6,4,1]
-]
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+
 */
 public class N119_PascalsTriangle2 {
 	//4 ms
@@ -42,8 +44,29 @@ public class N119_PascalsTriangle2 {
     			list.set(j+1, list.get(j) + list.get(j+1));
     		}
     		list.add(1);
-    	}     
+    	}
     	return list;
-    }    
-    
+    }
+
+
+    // version 3 added on 9/13/2016
+    // the index starts from 0?
+    // rolling array, scan backward.
+    // n=0    1
+    // n=1    1 1
+    // n=2    1 2 1
+    // n=3    1 3 3 1
+    // 1 ms 34 / 34 test cases passed.
+    public class Solution {
+        public List<Integer> getRow(int rowIndex) {
+            Integer[] row = new Integer[rowIndex+1];
+            Arrays.fill(row, 1);
+            for(int i=1; i<=rowIndex;i++){
+                for(int j= i-1;j >= 1;j--){  // only works if backward
+                    row[j] += row[j-1];
+                }
+            }
+            return Arrays.asList(row);
+        }
+    }
 }
