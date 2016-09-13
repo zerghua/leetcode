ea71749fa405a172c8740a31b08919b84756ae34
zerghua <zerghua@gmail.com>
2016-09-12 20:11:51 -0400

    114, flatten binary tree. Morris Algorithm?

--- leetcode/src/leetcode/N114_FlattenBinaryTreetoLinkedList.java
+++ leetcode/src/leetcode/N114_FlattenBinaryTreetoLinkedList.java
@@ -3,6 +3,37 @@ import leetcode.N0_data_strcture.*;
 
 /**
  * Created by Hua on 3/23/2016.
+
+ Given a binary tree, flatten it to a linked list in-place.
+
+ For example,
+ Given
+
+     1
+    / \
+   2   5
+  / \   \
+ 3   4   6
+
+ The flattened tree should look like:
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+
+ click to show hints.
+ Hints:
+
+ If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+
  */
 
 public class N114_FlattenBinaryTreetoLinkedList {
