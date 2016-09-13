1f638b8888eb3185fab68878c4e476aa7f15348c
zerghua <zerghua@gmail.com>
2016-09-13 15:57:41 -0400

    122, buy and sell stock as much as possible.greedy.  add up all positive difference.

--- leetcode/src/leetcode/N122_BestTimetoBuyandSellStock2.java
+++ leetcode/src/leetcode/N122_BestTimetoBuyandSellStock2.java
@@ -1,5 +1,12 @@
 package leetcode;
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
 
+Design an algorithm to find the maximum profit. You may complete as many transactions
+as you like (ie, buy one and sell one share of the stock multiple times). However,
+you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+ */
 public class N122_BestTimetoBuyandSellStock2 {
 	//2 ms
     public int maxProfit(int[] prices) { 
@@ -34,6 +41,20 @@ public class N122_BestTimetoBuyandSellStock2 {
             }
         }
         return profit;
-    }    
+    }
+
+    // version 3 added on 9/13/2016
+    // more concise code, greedy. add up all positive difference.
+    // 2 ms  198 / 198 test cases passed.
+    public class Solution {
+        public int maxProfit(int[] prices) {
+            if(prices == null || prices.length <= 1) return 0;
+            int ret =0;
+            for(int i=1;i<prices.length;i++){
+                ret += Math.max(0, prices[i]-prices[i-1]);
+            }
+            return ret;
+        }
+    }
     
 }
