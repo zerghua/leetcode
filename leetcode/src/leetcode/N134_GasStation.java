f5c530893c3d64f8a363959bf9c7cdb00cc21acc
zerghua <zerghua@gmail.com>
2016-09-13 20:40:59 -0400

    134, gas station. read problem description carefully, because guaranteed solution to be unique if failed at i, we can give up all start index [0,i], start checking from i+1.

--- leetcode/src/leetcode/N134_GasStation.java
+++ leetcode/src/leetcode/N134_GasStation.java
@@ -2,10 +2,24 @@ package leetcode;
 
 /**
  * Created by Hua on 4/3/2016.
+
+ There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+
+ You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i
+ to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+
+ Return the starting gas station's index if you can travel around the circuit once,
+ otherwise return -1.
+
+ Note:
+ The solution is guaranteed to be unique.
+
  */
 public class N134_GasStation {
-    //greedy
-    //find the start point which can circulate the cycle.
+    // greedy
+    // find the start point which can circulate the cycle.
+    // read problem description carefully, because guaranteed solution to be unique
+    // if failed at i, we can give up all start index [0,i], start checking from i+1.
     //1 ms
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int net_sum=0, cur_sum=0, start=0;
