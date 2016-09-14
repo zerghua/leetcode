59b2e14d12db50a93d84c5d608cd2eb61deaaed0
zerghua <zerghua@gmail.com>
2016-09-14 13:43:01 -0400

    141, linked list cycle. two pointers, fast and slow.

--- leetcode/src/leetcode/N141_LinkedListCycle.java
+++ leetcode/src/leetcode/N141_LinkedListCycle.java
@@ -1,8 +1,16 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+/*
 
+ Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+ */
 public class N141_LinkedListCycle {
 	//1 ms
+    // two pointers, fast and slow.
     public boolean hasCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
