e98c43bc223366f65326e72dd4bac7076574a69b
zerghua <zerghua@gmail.com>
2016-09-15 18:55:11 -0400

    165, compare version number. need to clarify problem. 222> 3.

--- leetcode/src/leetcode/N165_CompareVersionNumbers.java
+++ leetcode/src/leetcode/N165_CompareVersionNumbers.java
@@ -1,5 +1,18 @@
 package leetcode;
 /*
+
+Compare two version numbers version1 and version2.
+If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+
+You may assume that the version strings are non-empty and contain only digits and the . character.
+The . character does not represent a decimal point and is used to separate number sequences.
+For instance, 2.5 is not "two and a half" or "half way to version three",
+it is the fifth second-level revision of the second first-level revision.
+
+Here is an example of version numbers ordering:
+
+0.1 < 1.1 < 1.2 < 13.37
+
 public int compareVersion(String version1, String version2) {
     String[] arr1 = version1.split("\\.");
     String[] arr2 = version2.split("\\.");
@@ -30,6 +43,9 @@ public int compareVersion(String version1, String version2) {
  */
 public class N165_CompareVersionNumbers {
 	//5 ms
+    // test case:
+    // 1.22 >  1.3   (because 22 > 3)
+    // 1.22 <  1.101 (because 22 < 101)
     public static int compareVersion(String version1, String version2) {
         String[] v1_array = version1.split("\\.");
         String[] v2_array = version2.split("\\.");
@@ -54,8 +70,24 @@ public class N165_CompareVersionNumbers {
     
     public static void main(String[] args) {	
     	int ret =compareVersion("0", "1");
-    	
     	System.out.println(ret);
-    	
+    }
+
+    // potential issue when num1 or num2 > Integer.Max_Value
+    // 1 ms 71 / 71 test cases passed.
+    public class Solution {
+        public int compareVersion(String version1, String version2) {
+            int i=0,j=0;
+            char[] c1 = version1.toCharArray(), c2 = version2.toCharArray();
+            while(i<c1.length || j<c2.length){
+                int num1=0, num2=0;
+                while(i<c1.length && c1[i] != '.') num1 = num1*10 + c1[i++] -'0';
+                while(j<c2.length && c2[j] != '.') num2 = num2*10 + c2[j++] -'0';
+                if(num1 > num2) return 1;
+                if(num1 < num2) return -1;
+                i++;j++;
+            }
+            return 0;
+        }
     }
 }
