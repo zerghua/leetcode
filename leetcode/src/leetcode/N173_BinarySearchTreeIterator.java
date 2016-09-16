245f01585a471e52041d9760d93d4e734d645921
zerghua <zerghua@gmail.com>
2016-09-16 10:57:04 -0400

    173, binary search tree iterator. stack.

--- leetcode/src/leetcode/N173_BinarySearchTreeIterator.java
+++ leetcode/src/leetcode/N173_BinarySearchTreeIterator.java
@@ -5,6 +5,14 @@ import java.util.Stack;
 
 /**
  * Created by Hua on 3/20/2016.
+
+ Implement an iterator over a binary search tree (BST).
+ Your iterator will be initialized with the root node of a BST.
+
+ Calling next() will return the next smallest number in the BST.
+
+ Note: next() and hasNext() should run in average O(1) time and uses O(h) memory,
+ where h is the height of the tree.
  */
 public class N173_BinarySearchTreeIterator {
     Stack<TreeNode> st;
