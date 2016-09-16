56b5afa1a641a1ca59ba418eb0b64c006fd73368
zerghua <zerghua@gmail.com>
2016-09-16 10:20:35 -0400

    168, excel sheet column title. convert base 10 to base 26. construct from right to left, use mod first then divide. n-- to solve non-zero issue.

--- leetcode/src/leetcode/N168_ExcelSheetColumnTitle.java
+++ leetcode/src/leetcode/N168_ExcelSheetColumnTitle.java
@@ -1,7 +1,23 @@
 package leetcode;
+/*
 
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB
+
+ */
 public class N168_ExcelSheetColumnTitle {
 	//0 ms
+    // convert base 10 to base 26
+    // construct from right to left, use mod first then divide. n-- to solve non-zero issue.
     public static String convertToTitle(int n) {
     	String ret="";
         while(n>0){
