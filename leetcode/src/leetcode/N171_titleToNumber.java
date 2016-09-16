093896e8831431ca772db3733065b0b35ce975c1
zerghua <zerghua@gmail.com>
2016-09-16 10:34:31 -0400

    171, title to number. base 26 to base 10.

--- leetcode/src/leetcode/N171_titleToNumber.java
+++ leetcode/src/leetcode/N171_titleToNumber.java
@@ -1,5 +1,20 @@
 package leetcode;
+/*
+Given a column title as appear in an Excel sheet, return its corresponding column number.
 
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+
+ */
+
+// base 26 to base 10
 public class N171_titleToNumber {
 	//376 ms 20%
     public int titleToNumber(String s) {
