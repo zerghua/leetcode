d58e768518f79585488eaa394e3439a152ffe479
zerghua <zerghua@gmail.com>
2016-09-16 13:01:44 -0400

    191, count number of 1 bit.

--- leetcode/src/leetcode/N191_Numberof1Bits.java
+++ leetcode/src/leetcode/N191_Numberof1Bits.java
@@ -3,12 +3,12 @@ package leetcode;
 /**
  * Created by Hua on 7/10/2016.
  *
- * Write a function that takes an unsigned integer and
- * returns the number of ¡¯1' bits it has (also known as the Hamming weight).
+ Write a function that takes an unsigned integer and
+ returns the number of â€™1' bits it has (also known as the Hamming weight).
 
- For example, the 32-bit integer ¡¯11' has
- binary representation 00000000000000000000000000001011,
- so the function should return 3.
+ For example, the 32-bit integer â€™11' has
+ binary representation 00000000000000000000000000001011, so the function should return 3.
+
 
  Left shift (<<):  -2 << 1 == -4,   10000010 -->  10000100
  right shift (>>): -2 >> 1 == -1    10000010 -->  10000001
