fee7f8226bf80af7da84e45888fb6283b780c2c1
zerghua <zerghua@gmail.com>
2016-09-16 17:32:28 -0400

    200, number of islands. DFS, for each cell, do DFS on adjacent cells, and mark them as visited

--- leetcode/src/leetcode/N200_NumberofIslands.java
+++ leetcode/src/leetcode/N200_NumberofIslands.java
@@ -2,8 +2,33 @@ package leetcode;
 
 /**
  * Created by Hua on 4/3/2016.
+
+ Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ An island is surrounded by water and is formed by connecting adjacent lands
+ horizontally or vertically. You may assume all four edges of the grid are
+ all surrounded by water.
+
+ Example 1:
+
+ 11110
+ 11010
+ 11000
+ 00000
+
+ Answer: 1
+
+ Example 2:
+
+ 11000
+ 11000
+ 00100
+ 00011
+
+ Answer: 3
+
  */
-//["111","010","111"]
+    //["111","010","111"]
+    // DFS, for each cell, do DFS on adjacent cells, and mark them as visited.
 public class N200_NumberofIslands {
     //dfs, look at how predicate is pushed down, result a cleaner code
     public void dfs(char[][] grid, int i, int j,
