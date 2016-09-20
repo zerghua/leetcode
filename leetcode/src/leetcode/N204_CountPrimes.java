950e40d5f5ef53efa3735cef45cfefeb90321085
zerghua <zerghua@gmail.com>
2016-09-20 16:25:42 -0400

    204, count prime. math, DP. if dp[i] is prime, set dp[i*j] is not prime, for j [2, i*j<=n]

--- leetcode/src/leetcode/N204_CountPrimes.java
+++ leetcode/src/leetcode/N204_CountPrimes.java
@@ -3,50 +3,44 @@ package leetcode;
 /**
  * Created by Hua on 5/23/2016.
 
+ Description:
+
  Count the number of prime numbers less than a non-negative number, n.
+
 
  Hint:
 
- Let's start with a isPrime function. To determine if a number is prime,
- we need to check if it is not divisible by any number less than n.
- The runtime complexity of isPrime function would be O(n) and
- hence counting the total prime numbers up to n would be O(n2). Could we do better?
+ Let's start with a isPrime function. To determine if a number is prime, we need to check if it is not divisible by any number less than n. The runtime complexity of isPrime function would be O(n) and hence counting the total prime numbers up to n would be O(n2). Could we do better?
 
- As we know the number must not be divisible by any number > n / 2,
- we can immediately cut the total iterations half by dividing only up to n / 2.
- Could we still do better?
+ As we know the number must not be divisible by any number > n / 2, we can immediately cut the total iterations half by dividing only up to n / 2. Could we still do better?
 
  Let's write down all of 12's factors:
 
- 2 ¡Á 6 = 12
- 3 ¡Á 4 = 12
- 4 ¡Á 3 = 12
- 6 ¡Á 2 = 12
+ 2 Ã— 6 = 12
+ 3 Ã— 4 = 12
+ 4 Ã— 3 = 12
+ 6 Ã— 2 = 12
 
- As you can see, calculations of 4 ¡Á 3 and 6 ¡Á 2 are not necessary.
- Therefore, we only need to consider factors up to ¡Ìn because,
- if n is divisible by some number p, then n = p ¡Á q and since p ¡Ü q,
- we could derive that p ¡Ü ¡Ìn.
+ As you can see, calculations of 4 Ã— 3 and 6 Ã— 2 are not necessary. Therefore, we only need to consider factors up to âˆšn because, if n is divisible by some number p, then n = p Ã— q and since p â‰¤ q, we could derive that p â‰¤ âˆšn.
 
- Our total runtime has now improved to O(n1.5), which is slightly better.
- Is there a faster approach?
+ Our total runtime has now improved to O(n1.5), which is slightly better. Is there a faster approach?
 
  public int countPrimes(int n) {
- int count = 0;
- for (int i = 1; i < n; i++) {
- if (isPrime(i)) count++;
- }
- return count;
+     int count = 0;
+     for (int i = 1; i < n; i++) {
+         if (isPrime(i)) count++;
+         }
+     return count;
  }
 
  private boolean isPrime(int num) {
- if (num <= 1) return false;
- // Loop's ending condition is i * i <= num instead of i <= sqrt(num)
- // to avoid repeatedly calling an expensive function sqrt().
- for (int i = 2; i * i <= num; i++) {
- if (num % i == 0) return false;
- }
- return true;
+     if (num <= 1) return false;
+     // Loop's ending condition is i * i <= num instead of i <= sqrt(num)
+     // to avoid repeatedly calling an expensive function sqrt().
+     for (int i = 2; i * i <= num; i++) {
+         if (num % i == 0) return false;
+     }
+     return true;
  }
 
 
