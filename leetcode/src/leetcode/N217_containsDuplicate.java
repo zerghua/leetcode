604d76a353e4a93241b056ffdd4d05e1dc87ebb4
zerghua <zerghua@gmail.com>
2016-09-21 14:16:48 -0400

    217, contain duplicate. simple hash table

--- leetcode/src/leetcode/N217_containsDuplicate.java
+++ leetcode/src/leetcode/N217_containsDuplicate.java
@@ -1,5 +1,11 @@
 package leetcode;
 import java.util.HashSet;
+/*
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+ */
+
 
 //400 ms 93%
 public class N217_containsDuplicate {
