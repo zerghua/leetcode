420a6579bc69675bad3f602ce67c53165edeebac
zerghua <zerghua@gmail.com>
2016-09-21 17:34:59 -0400

    226, invert tree. add problem description.

--- leetcode/src/leetcode/N226_invertTree.java
+++ leetcode/src/leetcode/N226_invertTree.java
@@ -2,18 +2,36 @@ package leetcode;
 import java.util.Stack;
 
 import leetcode.N0_data_strcture.*;
+/*
 
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+ */
 public class N226_invertTree {
 	
 	//260ms 81%
     public TreeNode invertTree(TreeNode root) {
         if(root == null) return root;
-        else {
-        	TreeNode tmp = invertTree(root.left);
-        	root.left = invertTree(root.right);
-        	root.right = tmp;
-        	return root;        
-        }
+		else {
+			TreeNode tmp = invertTree(root.left);
+			root.left = invertTree(root.right);
+			root.right = tmp;
+			return root;
+		}
     }
     
     //iterative   268 ms 70%
