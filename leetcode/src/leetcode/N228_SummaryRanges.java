1aac4bf3574609410942df1ff254d586bab53279
zerghua <zerghua@gmail.com>
2016-09-21 17:48:37 -0400

    228, summary ranges. simple array operation.

--- leetcode/src/leetcode/N228_SummaryRanges.java
+++ leetcode/src/leetcode/N228_SummaryRanges.java
@@ -1,8 +1,15 @@
 package leetcode;
 import java.util.*;
-//[-2147483648,-2147483647,2147483647]
-//Given a sorted integer array without duplicates, return the summary of its ranges.
-//For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+/*
+ Given a sorted integer array without duplicates, return the summary of its ranges.
+
+For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+
+test case:
+[-2147483648,-2147483647,2147483647]
+ */
 
 // 1 ms
 public class N228_SummaryRanges {
