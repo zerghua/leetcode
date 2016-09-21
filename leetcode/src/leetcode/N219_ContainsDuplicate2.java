6e19e243f06160e704e733b7f69efd8106aa3231
zerghua <zerghua@gmail.com>
2016-09-21 14:20:53 -0400

    219, contains duplicate 2. duplicate within k distance.

--- leetcode/src/leetcode/N219_ContainsDuplicate2.java
+++ leetcode/src/leetcode/N219_ContainsDuplicate2.java
@@ -1,5 +1,11 @@
 package leetcode;
 import java.util.*;
+
+/*
+Given an array of integers and an integer k,
+find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+ */
+
 public class N219_ContainsDuplicate2 {
 	//13 ms
     public boolean containsNearbyDuplicate(int[] nums, int k) {
