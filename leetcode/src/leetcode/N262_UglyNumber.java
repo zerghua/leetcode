9264f565face02b7e163071944869aa18c36bec6
zerghua <zerghua@gmail.com>
2016-09-22 17:15:55 -0400

    262, ugly number. add problem description.

--- leetcode/src/leetcode/N262_UglyNumber.java
+++ leetcode/src/leetcode/N262_UglyNumber.java
@@ -1,5 +1,12 @@
 package leetcode;
+/*
+ Write a program to check whether a given number is an ugly number.
 
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number.
+
+ */
 
 public class N262_UglyNumber {
 	public int getRemain(int num, int divisor){
