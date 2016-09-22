478aad15af4c940fa52a4e561413714ecbc5d1e8
zerghua <zerghua@gmail.com>
2016-09-22 16:16:35 -0400

    237, delete node with only access to that node. modify node value.

--- leetcode/src/leetcode/N237_deleteNode.java
+++ leetcode/src/leetcode/N237_deleteNode.java
@@ -1,6 +1,13 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+/*
 
+ Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+the linked list should become 1 -> 2 -> 4 after calling your function.
+
+ */
 // 308 ms, 75%
 public class N237_deleteNode {
     public void deleteNode(ListNode node) {
