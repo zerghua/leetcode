1987ca05b67808290906e10df7a408e6381d5533
zerghua <zerghua@gmail.com>
2016-09-22 16:33:57 -0400

    240, search in a 2d matrix 2. add problem description.

--- leetcode/src/leetcode/N240_Searcha2DMatrix2.java
+++ leetcode/src/leetcode/N240_Searcha2DMatrix2.java
@@ -2,6 +2,28 @@ package leetcode;
 
 /**
  * Created by Hua on 3/18/2016.
+
+ Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+ Integers in each row are sorted in ascending from left to right.
+ Integers in each column are sorted in ascending from top to bottom.
+
+ For example,
+
+ Consider the following matrix:
+
+ [
+ [1,   4,  7, 11, 15],
+ [2,   5,  8, 12, 19],
+ [3,   6,  9, 16, 22],
+ [10, 13, 14, 17, 24],
+ [18, 21, 23, 26, 30]
+ ]
+
+ Given target = 5, return true.
+
+ Given target = 20, return false.
+
  */
 public class N240_Searcha2DMatrix2 {
     // binary search in each row, o(mlogn)  24%, means testing data is small
