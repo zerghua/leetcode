642be17f6fc84cd2ab268f4747ef8f72d2108823
zerghua <zerghua@gmail.com>
2016-09-23 16:26:41 -0400

    310, minimum height trees. update problem description.

--- leetcode/src/leetcode/N310_MinimumHeightTrees.java
+++ leetcode/src/leetcode/N310_MinimumHeightTrees.java
@@ -5,21 +5,28 @@ import java.util.*;
 /**
  * Created by Hua on 6/17/2016.
 
- For a undirected graph with tree characteristics, we can choose any node as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+ For a undirected graph with tree characteristics,
+ we can choose any node as the root. The result graph is then a rooted tree.
+ Among all possible rooted trees, those with minimum height are called
+ minimum height trees (MHTs). Given such a graph, write a function to find
+ all the MHTs and return a list of their root labels.
 
  Format
- The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number n and a list of undirected edges (each edge is a pair of labels).
+ The graph contains n nodes which are labeled from 0 to n - 1.
+ You will be given the number n anda list of undirected edges (each edge is a pair of labels).
 
- You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+ You can assume that no duplicate edges will appear in edges.
+ Since all edges are undirected, [0, 1] is the same as [1, 0]
+ and thus will not appear together in edges.
 
  Example 1:
 
  Given n = 4, edges = [[1, 0], [1, 2], [1, 3]]
 
- 0
- |
- 1
- / \
+   0
+   |
+   1
+  / \
  2   3
 
  return [1]
@@ -30,11 +37,11 @@ import java.util.*;
 
  0  1  2
  \ | /
-3
- |
- 4
- |
- 5
+   3
+   |
+   4
+   |
+   5
 
  return [3, 4]
 
@@ -45,11 +52,11 @@ import java.util.*;
  Note:
 
  (1) According to the definition of tree on Wikipedia:
- °∞a tree is an undirected graph in which any two vertices are connected
- by exactly one path. In other words, any connected graph without simple cycles is a tree.°±
+ ‚Äúa tree is an undirected graph in which any two vertices are connected by exactly one path.
+ In other words, any connected graph without simple cycles is a tree.‚Äù
 
- (2) The height of a rooted tree is the number of edges on the longest
- downward path between the root and a leaf.
+ (2) The height of a rooted tree is the number of edges on the longest downward path
+ between the root and a leaf.
 
 
  */
