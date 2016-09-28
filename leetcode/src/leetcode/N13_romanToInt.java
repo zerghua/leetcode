1c3fa21e60049f6e97e15eb43fc66d9170ad0d84
zerghua <zerghua@gmail.com>
2016-09-28 11:23:02 -0400

    13, roman to int. add problem description.

--- leetcode/src/leetcode/N13_romanToInt.java
+++ leetcode/src/leetcode/N13_romanToInt.java
@@ -1,6 +1,13 @@
 package leetcode;
-
 import java.util.HashMap;
+
+/*
+
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+ */
+
 
 public class N13_romanToInt {
 	
