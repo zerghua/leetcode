c26f70b55ff882abfc1b5f20a2c1cd70878ea825
zerghua <zerghua@gmail.com>
2016-09-30 10:45:22 -0400

    problem description

--- leetcode/src/leetcode/N112_PathSum.java
+++ leetcode/src/leetcode/N112_PathSum.java
@@ -2,7 +2,9 @@ package leetcode;
 import leetcode.N0_data_strcture.*;
 /*
 
- Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path
+such that adding up all the values along the path equals the given sum.
+
 For example:
 Given the below binary tree and sum = 22,
 
