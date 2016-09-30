566dd07a003ffcf8b7ae2788299565a4724abe9b
zerghua <zerghua@gmail.com>
2016-09-30 09:26:55 -0400

    72, edit distance. add problem description.

--- leetcode/src/leetcode/N72_EditDistance.java
+++ leetcode/src/leetcode/N72_EditDistance.java
@@ -2,6 +2,16 @@ package leetcode;
 
 /**
  * Created by Hua on 4/21/2016.
+
+ Given two words word1 and word2, find the minimum number of steps required
+ to convert word1 to word2. (each operation is counted as 1 step.)
+
+ You have the following 3 operations permitted on a word:
+
+ a) Insert a character
+ b) Delete a character
+ c) Replace a character
+
  */
 public class N72_EditDistance {
     //18 ms
