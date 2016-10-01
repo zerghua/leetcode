9999a7a97214459833b53cf26d5940a5c203a1e8
zerghua <zerghua@gmail.com>
2016-10-01 12:27:35 -0400

    75, sort color. add counting sort.

--- leetcode/src/leetcode/N75_SortColors.java
+++ leetcode/src/leetcode/N75_SortColors.java
@@ -41,6 +41,43 @@ public class N75_SortColors {
         }
     }
 
-    //can also do counting sort.
+    // added on 10/1/2016
+    // 0 ms 86 / 86 test cases passed.
+    public class Solution {
+        public void swap(int[] a, int i, int j){
+            int tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+        }
+
+        public void sortColors(int[] nums) {
+            int left=0, right=nums.length-1, i=0;
+            while(i<=right){
+                if(nums[i] == 0) {
+                    swap(nums, i++, left++);
+                }else if(nums[i] == 2){
+                    swap(nums, i, right--);
+                }else  i++;
+            }
+        }
+    }
+
+    //counting sort added on 10/1/2016
+    // 0 ms 86 / 86 test cases passed.
+    public class Solution2 {
+        public void sortColors(int[] nums) {
+            int zero=0, one=0, two=0;
+            for(int n:nums){
+                if(n==0) zero++;
+                else if (n==1) one++;
+                else two++;
+            }
+
+            int i=0;
+            while(zero-->0) nums[i++]=0;
+            while(one-->0) nums[i++]=1;
+            while(two-->0) nums[i++]=2;
+        }
+    }
 
 }
