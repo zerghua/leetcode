3481294fbdd170bb036002f230180bc9d89d41e4
zerghua <zerghua@gmail.com>
2016-10-02 14:41:33 -0400

    problem description.

--- leetcode/src/leetcode/N198_HouseRobber.java
+++ leetcode/src/leetcode/N198_HouseRobber.java
@@ -1,5 +1,15 @@
 package leetcode;
+/*
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed,
+the only constraint stopping you from robbing each of them is that
+adjacent houses have security system connected and it will automatically
+contact the police if two adjacent houses were broken into on the same night.
 
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+
+ */
 public class N198_HouseRobber {
 	//0 ms
     public int rob(int[] nums) {
