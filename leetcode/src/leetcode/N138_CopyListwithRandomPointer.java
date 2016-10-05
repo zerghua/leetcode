00003a3abc60ab68d2dd94992b7e93028a6b7612
zerghua <zerghua@gmail.com>
2016-10-05 14:05:44 -0400

    138, add problem description.

--- leetcode/src/leetcode/N138_CopyListwithRandomPointer.java
+++ leetcode/src/leetcode/N138_CopyListwithRandomPointer.java
@@ -2,6 +2,12 @@ package leetcode;
 
 /**
  * Created by Hua on 4/27/2016.
+
+ A linked list is given such that each node contains an additional random pointer
+ which could point to any node in the list or null.
+
+ Return a deep copy of the list.
+
  */
 public class N138_CopyListwithRandomPointer {
     public class RandomListNode {
