973fff59a9c79b139b6024584165f166d2db2813
zerghua <zerghua@gmail.com>
2016-10-04 20:13:39 -0400

    28, strstr. concise solution

--- leetcode/src/leetcode/N28_ImplementstrStr.java
+++ leetcode/src/leetcode/N28_ImplementstrStr.java
@@ -112,5 +112,18 @@ public class N28_ImplementstrStr {
         return next;
     }
 
+    // added on 10/4/2016  very concise solution
+    // 18 ms  72 / 72 test cases passed.
+    public class Solution {
+        public int strStr(String haystack, String needle) {
+            for(int i=0; ;i++){
+                for(int j=0; ;j++){
+                    if(j == needle.length()) return i;
+                    if(i+j == haystack.length()) return -1;
+                    if(haystack.charAt(i+j) != needle.charAt(j))break;
+                }
+            }
+        }
+    }
     
 }
