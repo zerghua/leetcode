833f873245ad94d2dec8218d15314b39dc2fe53b
zerghua <zerghua@gmail.com>
2016-10-08 12:05:24 -0400

    111, minimun depth of binary tree. add BFS solution.

--- leetcode/src/leetcode/N111_MinimumDepthofBinaryTree.java
+++ leetcode/src/leetcode/N111_MinimumDepthofBinaryTree.java
@@ -1,5 +1,8 @@
 package leetcode;
 import leetcode.N0_data_strcture.*;
+
+import java.util.*;
+
 /*
 Given a binary tree, find its minimum depth.
 
@@ -34,6 +37,57 @@ public class N111_MinimumDepthofBinaryTree {
             if(root.left == null || root.right == null)
                 return 1+Math.max(minDepth(root.left), minDepth(root.right));
             return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+    }
+
+    // add BFS solution on 10/8/2016
+    // BFS works well when tree is highly unbalanced
+    // 1 ms 41 / 41 test cases passed.
+    public class Solution_BFS {
+        public int minDepth(TreeNode root) {
+            if(root == null) return 0;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            int ret = 1;
+            boolean isFound=false;
+            while(!q.isEmpty()){
+                int size = q.size();
+                for(int i=0;i<size;i++){
+                    TreeNode node = q.poll();
+                    if(node.left == null && node.right==null) {
+                        isFound = true;
+                        break;
+                    }
+                    if(node.left != null) q.add(node.left);
+                    if(node.right!= null) q.add(node.right);
+                }
+                if(isFound) break;
+                ret++;
+            }
+            return ret;
+        }
+    }
+
+    // another BFS solution, use rightMost node as indicator of each level
+    // 2 ms 41 / 41 test cases passed.
+    public class Solution_BFS2 {
+        public int minDepth(TreeNode root) {
+            if(root == null) return 0;
+            Queue<TreeNode> q = new LinkedList<>();
+            q.add(root);
+            int ret = 1;
+            TreeNode rightMost=root;
+            while(!q.isEmpty()){
+                TreeNode node = q.poll();
+                if(node.left == null && node.right==null) break;
+                if(node.left != null) q.add(node.left);
+                if(node.right!= null) q.add(node.right);
+                if(node == rightMost){ // about to next level
+                    ret++;
+                    rightMost = (node.right == null)? node.left: node.right; // if node.left is null, meaning it's the last level.
+                }
+            }
+            return ret;
         }
     }
 }
