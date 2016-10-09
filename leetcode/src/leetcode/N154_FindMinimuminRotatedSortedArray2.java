cb04d720b6c072a4879c58e55d47d9cee458dbde
zerghua <zerghua@gmail.com>
2016-10-09 16:25:09 -0400

    154, add another solution.

--- leetcode/src/leetcode/N154_FindMinimuminRotatedSortedArray2.java
+++ leetcode/src/leetcode/N154_FindMinimuminRotatedSortedArray2.java
@@ -34,6 +34,22 @@ public class N154_FindMinimuminRotatedSortedArray2 {
         return nums[l];
     }
 
+    // optimized code added on 10/9/2016
+    // 1 ms 191 / 191 test cases passed.
+    public class Solution {
+        public int findMin(int[] nums) {
+            if(nums == null || nums.length==0) return -1;
+            int l=0, r=nums.length-1;
+            while(l<r && nums[l]>=nums[r]){
+                int mid = l + (r-l)/2;
+                if(nums[mid] == nums[r]) r--;
+                else if(nums[mid] < nums[r]) r = mid;   //highlight, not mid-1
+                else l = mid + 1;
+            }
+            return nums[l];
+        }
+    }
+
     public static void main(String[] args) {
         //int[] a= {4,5,6,7,2,2,2};
         //int[] a= {1,3,3};
