56a1bca902533dea366baac58908fe099dfc8d54
zerghua <zerghua@gmail.com>
2016-10-11 22:11:35 -0400

    340, to be done

---
+++ leetcode/src/leetcode/N340_LongestSubstringwithAtMostKDistinctCharacters.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+/**
+ * Created by HuaZ on 10/11/2016.
+
+ This is a problem asked by Google.
+
+ Given a string, find the longest substring that contains only two unique characters.
+ For example, given "abcbbbbcccbdddadacb", the longest substring that contains 2 unique character
+ is "bcbbbbcccb".
+ */
+public class N340_LongestSubstringwithAtMostKDistinctCharacters {
+}
