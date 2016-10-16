148728b77d290f010d222bb23c4b4ea5cfe96d5c
zerghua <zerghua@gmail.com>
2016-10-16 19:20:38 -0400

    rolling array solution.

--- leetcode/src/leetcode/N62_UniquePaths.java
+++ leetcode/src/leetcode/N62_UniquePaths.java
@@ -54,4 +54,19 @@ public class N62_UniquePaths {
         }
     }
 
+    // rolling 1D array added on 10/16/2016
+    // 1 ms 61 / 61 test cases passed.
+    public class Solution2 {
+        public int uniquePaths(int m, int n) {
+            if(m<=0 || n<=0) return 0;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            for(int i=0;i<m;i++){
+                for(int j=1;j<n;j++){
+                    dp[j] = dp[j] + dp[j-1];
+                }
+            }
+            return dp[n-1];
+        }
+    }
 }
