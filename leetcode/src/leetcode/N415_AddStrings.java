641837d0dad55a023b10ed7f4fc65e2d597b687a
zerghua <zerghua@gmail.com>
2016-10-16 16:43:26 -0400

    415, sum string. from right to left.

---
+++ leetcode/src/leetcode/N415_AddStrings.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * Created by HuaZ on 10/16/2016.
+
+ Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+
+ Note:
+
+ The length of both num1 and num2 is < 5100.
+ Both num1 and num2 contains only digits 0-9.
+ Both num1 and num2 does not contain any leading zero.
+ You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+
+ */
+public class N415_AddStrings {
+    public class Solution {
+        // from right to left
+        // 32 ms 315 / 315 test cases passed.
+        public String addStrings(String num1, String num2) {
+            StringBuilder ret = new StringBuilder();
+            int carry = 0, i=num1.length()-1, j=num2.length()-1;
+            while(i>=0 || j>=0){
+                int sum = carry;
+                if(i >= 0) {
+                    sum += num1.charAt(i) - '0';
+                    i--;
+                }
+                if(j >= 0) {
+                    sum += num2.charAt(j) - '0';
+                    j--;
+                }
+                carry = sum/10;
+                sum = sum%10;
+                ret.insert(0, sum);
+            }
+            if(carry>0) ret.insert(0,carry);
+            return ret.toString();
+        }
+    }
+
+}
