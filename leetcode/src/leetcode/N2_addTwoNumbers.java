5c78585b61297125260dd22898a776590f503882
zerghua <zerghua@gmail.com>
2016-10-16 20:02:10 -0400

    new solution.

--- leetcode/src/leetcode/N2_addTwoNumbers.java
+++ leetcode/src/leetcode/N2_addTwoNumbers.java
@@ -175,4 +175,29 @@ public class N2_addTwoNumbers {
             return dummy.next;
 		}
 	}
+
+	// added on 10/16/2016
+    // 50 ms 1559 / 1559 test cases passed.
+	public class Solution2 {
+		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			ListNode dummy= new ListNode(0), cur = dummy;
+			int carry=0;
+			while( l1!=null || l2 != null){
+			    int sum = carry;
+                if(l1 != null) {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if(l2 != null) {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                carry = sum/10;
+				cur.next = new ListNode(sum%10);
+				cur = cur.next;
+			}
+			if(carry != 0) cur.next = new ListNode(carry);
+			return dummy.next;
+		}
+	}
 }
