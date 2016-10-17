4d975435b9c66d1174cc89b66640ffbd3b9bde0a
zerghua <zerghua@gmail.com>
2016-10-17 12:25:32 -0400

    add a simpler solution.

--- leetcode/src/leetcode/N419_BattleshipsinaBoard.java
+++ leetcode/src/leetcode/N419_BattleshipsinaBoard.java
@@ -63,5 +63,26 @@ public class N419_BattleshipsinaBoard {
         }
     }
 
+    // simpler solution
+    // 4 ms 28 / 28 test cases passed.
+    public class Solution2 {
+        public int countBattleships(char[][] board) {
+            int m = board.length;
+            if (m==0) return 0;
+            int n = board[0].length;
+
+            int ret=0;
+            for (int i=0; i<m; i++) {
+                for (int j=0; j<n; j++) {
+                    if (board[i][j] == '.') continue;
+                    if (i > 0 && board[i-1][j] == 'X') continue;
+                    if (j > 0 && board[i][j-1] == 'X') continue;
+                    ret++;
+                }
+            }
+            return ret;
+        }
+    }
+
 
 }
