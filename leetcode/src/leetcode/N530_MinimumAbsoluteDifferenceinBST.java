6d9755d14c6d0d2ec1604126e8aca8b6605b6675
zerghua <zerghua@gmail.com>
2017-05-07 10:59:39 -0400

    530, minimum absolute difference in BST.

---
+++ leetcode/src/leetcode/N530_MinimumAbsoluteDifferenceinBST.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+/**
+ * Created by HuaZ on 5/7/2017.
+
+ Given a binary search tree with non-negative values, find the minimum absolute difference
+ between values of any two nodes.
+
+ Example:
+
+ Input:
+
+ 1
+  \
+   3
+  /
+ 2
+
+ Output:
+ 1
+
+ Explanation:
+ The minimum absolute difference is 1, which is the difference between 2 and 1 (or between 2 and 3).
+
+ Note: There are at least two nodes in this BST.
+
+ */
+import leetcode.N0_data_strcture.*;
+public class N530_MinimumAbsoluteDifferenceinBST {
+    // in-order traversal, one trick is to set pre = -1 and avoid the first diff.
+    // 186 / 186 test cases passed.
+    // 14 ms
+    public class Solution {
+        int ret = Integer.MAX_VALUE, pre = -1;
+        public int getMinimumDifference(TreeNode root) {
+            dfs(root);
+            return ret;
+        }
+
+        public void dfs(TreeNode node){
+            if(node == null) return ;
+            dfs(node.left);
+            if(pre != -1) ret = Math.min(ret, node.val - pre);
+            pre = node.val;
+            dfs(node.right);
+        }
+    }
+}
