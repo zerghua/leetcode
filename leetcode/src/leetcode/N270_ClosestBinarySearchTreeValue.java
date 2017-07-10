e3238bb6c88b0f3d9093be4aad2944812aa9a992
zerghua <zerghua@gmail.com>
2017-07-10 15:41:18 -0400

    270_ClosestBinarySearchTreeValue.

---
+++ leetcode/src/leetcode/N270_ClosestBinarySearchTreeValue.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * Created by Hua on 7/10/2017.
+
+ Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+
+ Note:
+
+ Given target value is a floating point.
+ You are guaranteed to have only one unique value in the BST that is closest to the target.
+
+ */
+
+import leetcode.N0_data_strcture.*;
+public class N270_ClosestBinarySearchTreeValue {
+    // 66 / 66 test cases passed.
+    // 1 ms
+    public class Solution {
+        int ret = 0;
+        double distance = Double.MAX_VALUE;
+
+        public int closestValue(TreeNode root, double target) {
+            dfs(root, target);
+            return ret;
+        }
+
+        public void dfs(TreeNode node, double target){
+            if(node == null) return;
+            if(Math.abs(node.val - target) < distance){
+                distance = Math.abs(node.val - target);
+                ret = node.val;
+            }
+            if(target > node.val) dfs(node.right, target);
+            else dfs(node.left, target);
+        }
+    }
+}
