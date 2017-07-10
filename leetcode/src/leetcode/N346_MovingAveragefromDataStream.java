288fdc0087b59078d2b815eeca0af5231ad49f9b
zerghua <zerghua@gmail.com>
2017-07-10 09:09:32 -0400

    346.MovingAveragefromDataStream. queue

---
+++ leetcode/src/leetcode/N346_MovingAveragefromDataStream.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+/**
+ * Created by Hua on 7/10/2017.
+
+ Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+
+ For example,
+
+ MovingAverage m = new MovingAverage(3);
+ m.next(1) = 1
+ m.next(10) = (1 + 10) / 2
+ m.next(3) = (1 + 10 + 3) / 3
+ m.next(5) = (10 + 3 + 5) / 3
+
+ */
+
+import java.util.*;
+public class N346_MovingAveragefromDataStream {
+    // 12 / 12 test cases passed.
+    // 160 ms
+    public class MovingAverage {
+        LinkedList<Integer> q;
+        int size;
+        long sum;
+
+        /** Initialize your data structure here. */
+        public MovingAverage(int size) {
+            q = new LinkedList();
+            this.size = size;
+            sum = 0;
+        }
+
+        public double next(int val) {
+            if(size == q.size()) sum -= q.removeFirst();
+
+            sum += val;
+            q.add(val);
+            return 1.0 * sum / q.size();
+        }
+    }
+}
