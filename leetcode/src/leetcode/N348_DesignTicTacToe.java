1927f86fd370afbd311b4eece3421b8d8d34e4cb
zerghua <zerghua@gmail.com>
2017-07-15 15:20:25 -0400

    348_DesignTicTacToe. 1 and -1 design

---
+++ leetcode/src/leetcode/N348_DesignTicTacToe.java
@@ -0,0 +1,106 @@
+package leetcode;
+
+/**
+ * Created by HuaZ on 7/15/2017.
+
+ Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+ You may assume the following rules:
+
+ A move is guaranteed to be valid and is placed on an empty block.
+ Once a winning condition is reached, no more moves is allowed.
+ A player who succeeds in placing n of their marks in a horizontal, vertical,
+ or diagonal row wins the game.
+
+ Example:
+
+ Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+ TicTacToe toe = new TicTacToe(3);
+
+ toe.move(0, 0, 1); -> Returns 0 (no one wins)
+ |X| | |
+ | | | |    // Player 1 makes a move at (0, 0).
+ | | | |
+
+ toe.move(0, 2, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ | | | |    // Player 2 makes a move at (0, 2).
+ | | | |
+
+ toe.move(2, 2, 1); -> Returns 0 (no one wins)
+ |X| |O|
+ | | | |    // Player 1 makes a move at (2, 2).
+ | | |X|
+
+ toe.move(1, 1, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ | |O| |    // Player 2 makes a move at (1, 1).
+ | | |X|
+
+ toe.move(2, 0, 1); -> Returns 0 (no one wins)
+ |X| |O|
+ | |O| |    // Player 1 makes a move at (2, 0).
+ |X| |X|
+
+ toe.move(1, 0, 2); -> Returns 0 (no one wins)
+ |X| |O|
+ |O|O| |    // Player 2 makes a move at (1, 0).
+ |X| |X|
+
+ toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+ |X| |O|
+ |O|O| |    // Player 1 makes a move at (2, 1).
+ |X|X|X|
+
+ Follow up:
+ Could you do better than O(n2) per move() operation?
+
+ */
+public class N348_DesignTicTacToe {
+    // the key is to set player 1 as 1, whicl player 2 as -1.
+    // o(1) move
+    // 32 / 32 test cases passed.
+    // 107 ms
+    public class TicTacToe {
+        int[] rows;
+        int[] cols;
+        int diagonal;
+        int anti_diagonal;
+        /** Initialize your data structure here. */
+        public TicTacToe(int n) {
+            rows = new int[n];
+            cols = new int[n];
+            diagonal = anti_diagonal = 0;
+        }
+
+        /** Player {player} makes a move at ({row}, {col}).
+         @param row The row of the board.
+         @param col The column of the board.
+         @param player The player, can be either 1 or 2.
+         @return The current winning condition, can be either:
+         0: No one wins.
+         1: Player 1 wins.
+         2: Player 2 wins. */
+        public int move(int row, int col, int player) {
+            int num = (player == 1) ? 1: -1;  // the most important design part
+            int n = rows.length;
+            rows[row] += num;
+            cols[col] += num;
+            if(row == col) diagonal += num;
+            if(row == n - col - 1) anti_diagonal += num;
+
+            if(Math.abs(rows[row]) == n || Math.abs(cols[col]) == n
+                    || Math.abs(diagonal) == n || Math.abs(anti_diagonal) == n){
+                return player;
+            }
+            return 0;
+        }
+    }
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
+}
