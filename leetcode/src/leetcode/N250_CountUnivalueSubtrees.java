5e5af516463968c38afff943aa9def44731b27fe
Hua <zerghua@gmail.com>
2017-07-17 08:50:51 -0400

    250_CountUnivalueSubtrees. regular dfs.

---
+++ leetcode/src/leetcode/N250_CountUnivalueSubtrees.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+/**
+ * Created by Hua on 7/17/2017.
+
+ Given a binary tree, count the number of uni-value subtrees.
+
+ A Uni-value subtree means all nodes of the subtree have the same value.
+
+ For example:
+ Given binary tree,
+
+     5
+    / \
+   1   5
+  / \   \
+ 5   5   5
+
+ return 4.
+
+ */
+
+import leetcode.N0_data_strcture.*;
+public class N250_CountUnivalueSubtrees {
+    // 197 / 197 test cases passed.
+    // 1 ms
+    // regular dfs
+    public class Solution {
+        int ret = 0;
+        public int countUnivalSubtrees(TreeNode root) {
+            dfs(root);
+            return ret;
+        }
+
+        public boolean dfs(TreeNode node){
+            if(node == null) return true;
+
+            boolean isLeftUnival = dfs(node.left);
+            boolean isRightUnival = dfs(node.right);
+            if(isLeftUnival && isRightUnival && (node.left == null || node.left.val == node.val)
+                    && (node.right == null || node.right.val == node.val)){
+                ret++;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
