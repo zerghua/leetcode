5c178a2b91ead99112b914f2728d43a4f86e7c76
zerghua <zerghua@gmail.com>
2017-07-22 16:05:51 -0400

    356_LineReflection. math + hashtable

---
+++ leetcode/src/leetcode/N356_LineReflection.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.HashSet;
+
+/**
+ * Created by HuaZ on 7/22/2017.
+
+ Given n points on a 2D plane, find if there is such a line parallel to y-axis
+ that reflect(对称) the given points.
+
+ Example 1:
+
+ Given points = [[1,1],[-1,1]], return true.
+
+ Example 2:
+
+ Given points = [[1,1],[-1,-1]], return false.
+
+ Follow up:
+ Could you do better than O(n2)?
+
+ */
+public class N356_LineReflection {
+    // google
+    // math and hashtable
+    // 37 / 37 test cases passed.
+    // 21 ms
+    public class Solution {
+        public boolean isReflected(int[][] points) {
+            HashSet<String> set = new HashSet();
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for(int[] p : points){
+                min = Math.min(min, p[0]);
+                max = Math.max(max, p[0]);
+                set.add(p[0] + ":" + p[1]);
+            }
+
+            int sum = min + max;
+            for(int[] p : points){
+                if(!set.contains((sum - p[0]) + ":" + p[1])) return false;
+            }
+            return true;
+        }
+    }
+}
