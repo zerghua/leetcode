25eb49960953545c9804d16beed96475620acce4
zerghua <zerghua@gmail.com>
2017-07-30 14:51:02 -0400

    add company info

--- leetcode/src/leetcode/N287_FindtheDuplicateNumber.java
+++ leetcode/src/leetcode/N287_FindtheDuplicateNumber.java
@@ -19,8 +19,18 @@ package leetcode;
  // 1,2,2,3
  // 0,1,2,4,2,3
 
+ The main idea is the same with problem Linked List Cycle II,
+ https://leetcode.com/problems/linked-list-cycle-ii/. Use two pointers the fast and the slow.
+ The fast one goes forward two steps each time, while the slow one goes only step each time.
+ They must meet the same item when slow==fast. In fact, they meet in a circle,
+ the duplicate number must be the entry point of the circle when visiting the array from nums[0].
+ Next we just need to find the entry point. We use a point(we can use the fast one before) to visit
+ form begining with one step each time, do the same job to slow. When fast==slow,
+ they meet at the entry point of the circle. The easy understood code is as follows.
+
  */
 public class N287_FindtheDuplicateNumber {
+    // bloomberg
     // o(nlogn) time binary search, pigeonhole principle
     // 21 ms
     public int findDuplicate(int[] nums) {
