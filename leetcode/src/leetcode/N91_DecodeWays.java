99940a5d93ed9287de8b4c0690d2c073519a41c4
Hua <zerghua@gmail.com>
2017-08-01 10:49:13 -0400

    91_DecodeWays. add company info

--- leetcode/src/leetcode/N91_DecodeWays.java
+++ leetcode/src/leetcode/N91_DecodeWays.java
@@ -20,8 +20,36 @@ import java.util.*;
 
  */
 public class N91_DecodeWays {
-    //TLE, but it saved all the decode ways
+    // facebook, microsoft, Uber
+    // DP
+    // XY could decode to: h(x), h(y), h(xy)
+    // h(y) = h(x) + h(xy) if valid,  y= x+1
+    // f(n) = f(n-1) + f(n-2) if valid
+    //7 ms
     public int numDecodings(String s) {
+        if(s == null || s.length()==0 || s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[s.length()+1];
+        dp[0]=1; dp[1]=1;
+        for(int i=2; i<=s.length();i++){
+            //check y
+            if(isValid(s.substring(i-1, i))) dp[i] += dp[i-1];
+
+            //check xy
+            if(isValid(s.substring(i-2, i))) dp[i] += dp[i-2];
+        }
+        return dp[s.length()];
+    }
+
+    public boolean isValid(String s){
+        if(s.charAt(0) == '0') return false;
+        return Integer.valueOf(s) >=1 && Integer.valueOf(s)<=26;
+    }
+
+
+
+    //TLE, but it saved all the decode ways
+    public int numDecodings2(String s) {
         List<LinkedList<String>> ret = new LinkedList<LinkedList<String>>();
         if(s == null || s.length()==0) return 0;
 
@@ -58,30 +86,7 @@ public class N91_DecodeWays {
         return ret.size();
     }
 
-    //DP
-    // XY could decode to: h(x), h(y), h(xy)
-    // h(y) = h(x) + h(xy) if valid,  y= x+1
-    // f(n) = f(n-1) + f(n-2) if valid
-    //7 ms
-    public int numDecodings2(String s) {
-        if(s == null || s.length()==0 || s.charAt(0) == '0') return 0;
 
-        int[] dp = new int[s.length()+1];
-        dp[0]=1; dp[1]=1;
-        for(int i=2; i<=s.length();i++){
-            //check y
-            if(isValid(s.substring(i-1, i))) dp[i] += dp[i-1];
-
-            //check xy
-            if(isValid(s.substring(i-2, i))) dp[i] += dp[i-2];
-        }
-        return dp[s.length()];
-    }
-
-    public boolean isValid(String s){
-        if(s.charAt(0) == '0') return false;
-        return Integer.valueOf(s) >=1 && Integer.valueOf(s)<=26;
-    }
 
 
 }
