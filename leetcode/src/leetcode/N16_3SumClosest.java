353d8a770bae144e77494b8b35bbbbad7e0f8d32
zerghua <zerghua@gmail.com>
2017-08-06 20:24:14 -0400

    add company info

--- leetcode/src/leetcode/N16_3SumClosest.java
+++ leetcode/src/leetcode/N16_3SumClosest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 
  */
 public class N16_3SumClosest {
+    // Bloomberg
     // o(n^2) time,
     // assume there is a solution
     public int threeSumClosest(int[] nums, int target) {
