b47383365bb8f1ff1997a84064eefcfdd617bbd5
Hua <zerghua@gmail.com>
2017-08-14 19:39:33 -0400

    update

--- leetcode/src/leetcode/N301_RemoveInvalidParentheses.java
+++ leetcode/src/leetcode/N301_RemoveInvalidParentheses.java
@@ -20,6 +20,36 @@ import java.util.*;
  */
 public class N301_RemoveInvalidParentheses {
     // Facebook
+    // https://discuss.leetcode.com/topic/34875/easy-short-concise-and-fast-java-dfs-3-ms-solution
+    // DFS
+    // 125 / 125 test cases passed.
+    // 3ms
+    public class Solution {
+        public List<String> removeInvalidParentheses(String s) {
+            List<String> ans = new ArrayList<>();
+            remove(s, ans, 0, 0, new char[]{'(', ')'});
+            return ans;
+        }
+
+        public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
+            for (int stack = 0, i = last_i; i < s.length(); ++i) {
+                if (s.charAt(i) == par[0]) stack++;
+                if (s.charAt(i) == par[1]) stack--;
+                if (stack >= 0) continue;
+                for (int j = last_j; j <= i; ++j)
+                    if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
+                        remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+                return;
+            }
+            String reversed = new StringBuilder(s).reverse().toString();
+            if (par[0] == '(') // finished left to right
+                remove(reversed, ans, 0, 0, new char[]{')', '('});
+            else // finished right to left
+                ans.add(reversed);
+        }
+    }
+
+
     // BFS, remove chars level by level. (remove 0 ~ n chars)
     // 103 ms
     public List<String> removeInvalidParentheses(String s) {
@@ -61,31 +91,6 @@ public class N301_RemoveInvalidParentheses {
         return count == 0;
     }
 
-    // https://discuss.leetcode.com/topic/34875/easy-short-concise-and-fast-java-dfs-3-ms-solution
-    // 3ms
-    class dfs{
-        public List<String> removeInvalidParentheses(String s) {
-            List<String> ans = new ArrayList<>();
-            remove(s, ans, 0, 0, new char[]{'(', ')'});
-            return ans;
-        }
 
-        public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
-            for (int stack = 0, i = last_i; i < s.length(); ++i) {
-                if (s.charAt(i) == par[0]) stack++;
-                if (s.charAt(i) == par[1]) stack--;
-                if (stack >= 0) continue;
-                for (int j = last_j; j <= i; ++j)
-                    if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
-                        remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
-                return;
-            }
-            String reversed = new StringBuilder(s).reverse().toString();
-            if (par[0] == '(') // finished left to right
-                remove(reversed, ans, 0, 0, new char[]{')', '('});
-            else // finished right to left
-                ans.add(reversed);
-        }
-    }
 
 }
