b861e72293c4f0f8a63dc96fe44522d863b92bda
zerghua <zerghua@gmail.com>
2017-08-16 22:09:48 -0400

    update 1

--- leetcode/src/leetcode/N218_TheSkylineProblem.java
+++ leetcode/src/leetcode/N218_TheSkylineProblem.java
@@ -39,9 +39,44 @@ import java.util.*;
  For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable;
  the three lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
 
+
+
+ Explain:
+ Firstly, please notice what we need to achieve:
+
+     1. visit all start points and all end points in order;
+     2. when visiting a point, we need to know whether it is a start point or a end point,
+        based on which we can add a height or delete a height from our data structure;
+
+ To achieve this, his implementation:
+
+     1. use a int[][] to collect all [start point, - height] and [end point, height] for every building;
+     2. sort it, firstly based on the first value, then use the second to break ties;
+
+ Thus,
+
+     1. we can visit all points in order;
+     2. when points have the same value, higher height will shadow the lower one;
+     3. we know whether current point is a start point or a end point based on the sign of its height;
+
+
+ Thus, our algorithm could be summarised in following pseudo code:
+
+     for position in sorted(all start points and all end points)
+         if this position is a start point
+            add its height
+         else if this position is a end point
+            delete its height
+         compare current max height with previous max height, if different, add
+         current position together with this new max height to our result, at the
+         same time, update previous max height to current max height;
+
+
+
+
  */
 public class N218_TheSkylineProblem {
-    // Microsoft, Google, Facebook
+    // Microsoft, Google, Facebook, Amazon
     // hard on understanding the problem definition and put it into code
     // the key is to find top left of each rectangle and return the proper ones + bottom rightmost corner.
     // 36 / 36 test cases passed.
@@ -50,21 +85,22 @@ public class N218_TheSkylineProblem {
         public List<int[]> getSkyline(int[][] buildings) {
             List<int[]> h = new ArrayList();  // each building is decomposed as two (x,y)
             for(int[] b: buildings){
-                h.add(new int[]{b[0], -b[2]});  // left  x is negative height, this is a trick.
+                h.add(new int[]{b[0], -b[2]});  // left  x is negative height, this is a trick, see reason in sort h.
                 h.add(new int[]{b[1], b[2]});   // right x is positive height
             }
 
-            // important to avoid return extra ret when two points have the same height. example: [0,2,3] and [2,5,3].
+            // important to avoid return extra ret when two points have the same height. example: [0,2,8] and [2,4,8].
             Collections.sort(h, (a,b) -> (a[0] == b[0])? a[1] - b[1] : a[0] - b[0]);
 
+            // global height, only return point when global height changes.
             PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
-            heap.add(0);
+            heap.add(0);  //also important to get the last point
 
             List<int[]> ret = new ArrayList<>();
             int preHeight = 0;
             for(int[] a: h){
-                if(a[1] < 0) heap.add(-a[1]);  // add left corner, problem definition
-                else heap.remove(a[1]);
+                if(a[1] < 0) heap.add(-a[1]);  // a start point, add global height
+                else heap.remove(a[1]);        // a end point, remove global height
 
                 if(heap.peek() != preHeight) ret.add(new int[]{a[0], heap.peek()});
                 preHeight = heap.peek();
