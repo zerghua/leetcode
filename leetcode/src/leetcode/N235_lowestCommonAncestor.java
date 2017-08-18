fe4022f2d15c14e33e7126d70a1eecd217b3ff03
Hua <zerghua@gmail.com>
2017-08-18 15:19:37 -0400

    update 5

--- leetcode/src/leetcode/N235_lowestCommonAncestor.java
+++ leetcode/src/leetcode/N235_lowestCommonAncestor.java
@@ -25,7 +25,23 @@ Another example is LCA of nodes 2 and 4 is 2, since a node can be a descendant o
 //this is Binary Search Tree, can be improved
 public class N235_lowestCommonAncestor {
     // Amazon, Microsoft, Facebook
-	//504 ms 33% 
+    // BST property
+    // 27 / 27 test cases passed.
+    // 8 ms
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            while(root!=null){
+                if(root.val < p.val && root.val < q.val)       root = root.right;
+                else if(root.val > p.val && root.val > q.val)  root = root.left;
+                else return root;
+            }
+            return null;
+        }
+    }
+
+
+	// 504 ms 33%
+    // this is for general binary tree, BST can be faster, see above
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null) return null;
         if(root == p || root == q) return root;
@@ -38,13 +54,6 @@ public class N235_lowestCommonAncestor {
     }	
     
     // 488 ms 50%
-    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
-    	while(root!=null){
-    		if(root.val < p.val && root.val < q.val)       root = root.right;
-    		else if(root.val > p.val && root.val > q.val)  root = root.left;    			
-    		else return root;
-    	}
-    	return null;
-    }	    
+
     
 }
--- leetcode/src/leetcode/N69_SqrtFunction.java
+++ leetcode/src/leetcode/N69_SqrtFunction.java
@@ -10,23 +10,26 @@ package leetcode;
  */
 public class N69_SqrtFunction {
     // Facebook, Bloomberg
-    // 1017 / 1017 test cases passed.
-    // 2 ms
-    // TLE on 2147483647?
+    // binary search
     // need to use long type for mid*mid, or possibly over flow.
-    public int mySqrt(int x) {
-        if(x<=1) return x; // it seems OJ treat this way, not mathematically right
+    // 1017 / 1017 test cases passed.  on 8/18/2017
+    // 2 ms
+    class Solution {
+        public int mySqrt(int x) {
+            if(x<=1) return x; // it seems OJ treat this way, not mathematically right
 
-        int ret=1, left=1, right=x;
-        while(left<=right){
-            int mid = (right-left)/2 + left;
-            long squareOfMid = (long)mid*mid;
-            if(squareOfMid == x) return mid;
-            else if(squareOfMid < x){
-                ret = mid;
-                left = mid+1;
-            }else right = mid-1;
+            int ret=1, left=1, right=x;
+            while(left<=right){
+                int mid = (right-left)/2 + left;
+                long squareOfMid = (long)mid*mid;
+
+                if(squareOfMid == x) return mid;
+                else if(squareOfMid < x){
+                    ret = mid;
+                    left = mid+1;
+                }else right = mid-1;
+            }
+            return ret;
         }
-        return ret;
     }
 }
