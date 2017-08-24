83388abba7098b7b710a212a4806a962a359f25a
zerghua <zerghua@gmail.com>
2017-08-23 22:26:55 -0400

    graph

--- leetcode/src/leetcode/N399_EvaluateDivision.java
+++ leetcode/src/leetcode/N399_EvaluateDivision.java
@@ -6,15 +6,17 @@ import java.util.HashSet;
 /**
  * Created by Hua on 5/22/2017.
 
- Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number
- (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+ Equations are given in the format A / B = k, where A and B are variables represented as strings,
+ and k is a real number(floating point number).
+ Given some queries, return the answers. If the answer does not exist, return -1.0.
 
  Example:
  Given a / b = 2.0, b / c = 3.0.
  queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
  return [6.0, 0.5, -1.0, 1.0, -1.0 ].
 
- The input is: vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries ,
+ The input is:
+ vector<pair<string, string>> equations, vector<double>& values, vector<pair<string, string>> queries,
  where equations.size() == values.size(), and the values are positive. This represents the equations.
  Return vector<double>.
 
@@ -66,8 +68,8 @@ public class N399_EvaluateDivision {
             set.add(key);
 
             for(String a_neighbour : map.get(a).keySet()){
-                double ret = dfs(a_neighbour, b, map, set);
-                if(ret != -1.0) return ret *  map.get(a).get(a_neighbour);
+                double ret = dfs(a_neighbour, b, map, set);    // ret = a_neighbour/b
+                if(ret != -1.0) return ret *  map.get(a).get(a_neighbour); // map.get(a).get(a_neighbour) = a/a_neighbour
             }
 
             set.remove(key);
