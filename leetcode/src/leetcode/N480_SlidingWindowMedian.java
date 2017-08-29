995b5f6a8da3c1037be45bc1e3522641566bf78f
zerghua <zerghua@gmail.com>
2017-08-29 17:30:46 -0400

    update 10

--- leetcode/src/leetcode/N480_SlidingWindowMedian.java
+++ leetcode/src/leetcode/N480_SlidingWindowMedian.java
@@ -6,7 +6,8 @@ import java.util.TreeSet;
 /**
  * Created by HuaZ on 6/3/2017.
 
- Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+ Median is the middle value in an ordered integer list. If the size of the list is even,
+ there is no middle value. So the median is the mean of the two middle value.
  Examples:
 
  [2,3,4] , the median is 3
