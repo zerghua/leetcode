d6d75e985dde995d94e26e4117721786f5cfca04
Hua <zerghua@gmail.com>
2017-08-30 17:37:39 -0400

    update 5

--- leetcode/src/leetcode/N53_MaximumSubarray.java
+++ leetcode/src/leetcode/N53_MaximumSubarray.java
@@ -3,22 +3,18 @@ package leetcode;
 /**
  Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
- For example, given the array [?2,1,?3,4,?1,2,1,?5,4],
- the contiguous subarray [4,?1,2,1] has the largest sum = 6.
+ For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+ the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+
+
+ More practice:
+ If you have figured out the O(n) solution,
+ try coding another solution using the divide and conquer approach, which is more subtle.
+
  */
 public class N53_MaximumSubarray {
     // Microsoft
-    // 1 ms
-    public int maxSubArray(int[] nums) {
-        int ret=Integer.MIN_VALUE, local_max=0;
-        for(int i : nums){
-            local_max += nums[i];
-            if(local_max > ret) ret = local_max;
-            if(local_max < 0) local_max=0;
-        }
-        return ret;
-    }
-
     // solution added on 10/9/2016
     // 16 ms 201 / 201 test cases passed.
     public class Solution {
@@ -31,4 +27,18 @@ public class N53_MaximumSubarray {
             return ret;
         }
     }
+
+
+    // 1 ms
+    public int maxSubArray(int[] nums) {
+        int ret=Integer.MIN_VALUE, local_max=0;
+        for(int i : nums){
+            local_max += nums[i];
+            if(local_max > ret) ret = local_max;
+            if(local_max < 0) local_max=0;
+        }
+        return ret;
+    }
+
+
 }
