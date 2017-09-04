d339b3a634e374b4114f48e409a052c7303c8eeb
zerghua <zerghua@gmail.com>
2017-09-03 20:14:34 -0400

    dp 2d.

--- leetcode/src/leetcode/N474_OnesandZeroes.java
+++ leetcode/src/leetcode/N474_OnesandZeroes.java
@@ -3,10 +3,11 @@ package leetcode;
 /**
  * Created by Hua on 5/18/2017.
 
- In the computer world, use restricted resource you have to generate maximum benefit is what we always want to pursue.
+ In the computer world, use restricted resource you have to
+ generate maximum benefit is what we always want to pursue.
 
- For now, suppose you are a dominator of m 0s and n 1s respectively. On the other hand, there is an array with strings
- consisting of only 0s and 1s.
+ For now, suppose you are a dominator of m 0s and n 1s respectively.
+ On the other hand, there is an array with strings consisting of only 0s and 1s.
 
  Now your task is to find the maximum number of strings that you can form with given m 0s and n 1s.
  Each 0 and 1 can be used at most once.
