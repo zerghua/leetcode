52477f79b8b8e559d5b38e4c775541faed3b641d
Hua <zerghua@gmail.com>
2017-09-05 12:49:33 -0400

    anagram and array

--- leetcode/src/leetcode/N48_RotateImage.java
+++ leetcode/src/leetcode/N48_RotateImage.java
@@ -12,14 +12,76 @@ package leetcode;
 
  this one needs analyze and code.
 
+
+
+
+     * clockwise rotate
+     * first reverse up to down, then swap the symmetry
+     * 1 2 3     7 8 9     7 4 1
+     * 4 5 6  => 4 5 6  => 8 5 2
+     * 7 8 9     1 2 3     9 6 3
+
+    void rotate(vector<vector<int> > &matrix) {
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = i + 1; j < matrix[i].size(); ++j)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+
+     * anticlockwise rotate
+     * first reverse left to right, then swap the symmetry
+     * 1 2 3     3 2 1     3 6 9
+     * 4 5 6  => 6 5 4  => 2 5 8
+     * 7 8 9     9 8 7     1 4 7
+
+    void anti_rotate(vector<vector<int> > &matrix) {
+        for (auto vi : matrix) reverse(vi.begin(), vi.end());
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = i + 1; j < matrix[i].size(); ++j)
+                swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+
  */
 public class N48_RotateImage {
     // Microsoft, Amazon, Apple
+    // reverse and swap, see reason above
+    // 21 / 21 test cases passed.  on 9/5/2017
+    // 3 ms
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length, i=0, j = n-1;
+            // reverse each row
+            while(i < j){
+                int[] tmp = matrix[i];
+                matrix[i] = matrix[j];
+                matrix[j] = tmp;
+                i++;j--;
+            }
+
+            // swap each char in diagonal
+            for(i=0; i<n; i++){
+                for(j = i+1; j<n; j++){
+                    int tmp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = tmp;
+                }
+            }
+        }
+    }
+
+
+
+
     // 20 / 20 test cases passed.
     // 2 ms
     //https://en.wikipedia.org/wiki/In-place_matrix_transposition
     //https://algorithmstuff.wordpress.com/2013/06/15/rotate-a-matrix-by-90-degrees/
-    public class Solution {
+    // 4-way rotation
+    public class Solution2 {
         public void rotate(int[][] matrix) {
             int n=matrix.length;
             int layers=n/2;
--- leetcode/src/leetcode/N59_SpiralMatrix2.java
+++ leetcode/src/leetcode/N59_SpiralMatrix2.java
@@ -16,30 +16,6 @@ You should return the following matrix:
  */
 public class N59_SpiralMatrix2 {
     // no company
-    //0 ms 14.9%
-    public int[][] generateMatrix(int n) {
-        if(n<1) return new int[0][0];
-        
-        int nums=1;
-        int[][] matrix = new int[n][n];
-        int row_start=0, row_end=n-1, col_start=0, col_end=n-1;
-        while(nums<=n*n){
-            for(int i=col_start; i<=col_end; i++) matrix[row_start][i] = nums++;
-            row_start++;
-            
-            for(int i=row_start; i<=row_end; i++) matrix[i][col_end] = nums++;
-            col_end--;
-            
-            for(int i=col_end; i>=col_start; i--) matrix[row_end][i] = nums++;
-            row_end--;
-            
-            for(int i=row_end; i>=row_start; i--) matrix[i][col_start] = nums++;
-            col_start++;
-        }
-        return matrix;
-    }
-
-
     // 0 ms  21 / 21 test cases passed.
     // very similar to Spiral Matrix 54, 4 sides of 4 loops.
     public class Solution {
@@ -67,5 +43,31 @@ public class N59_SpiralMatrix2 {
             return ret;
         }
     }
+
+    //0 ms 14.9%
+    public int[][] generateMatrix(int n) {
+        if(n<1) return new int[0][0];
+        
+        int nums=1;
+        int[][] matrix = new int[n][n];
+        int row_start=0, row_end=n-1, col_start=0, col_end=n-1;
+        while(nums<=n*n){
+            for(int i=col_start; i<=col_end; i++) matrix[row_start][i] = nums++;
+            row_start++;
+            
+            for(int i=row_start; i<=row_end; i++) matrix[i][col_end] = nums++;
+            col_end--;
+            
+            for(int i=col_end; i>=col_start; i--) matrix[row_end][i] = nums++;
+            row_end--;
+            
+            for(int i=row_end; i>=row_start; i--) matrix[i][col_start] = nums++;
+            col_start++;
+        }
+        return matrix;
+    }
+
+
+
 
 }
