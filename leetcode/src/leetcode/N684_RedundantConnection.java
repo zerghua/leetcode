9fb4e9ee23ea3081d99eca4c8512d03fb2f335a4
Hua <zerghua@gmail.com>
2017-09-26 18:53:10 -0400

    update

--- leetcode/src/leetcode/N684_RedundantConnection.java
+++ leetcode/src/leetcode/N684_RedundantConnection.java
@@ -3,41 +3,51 @@ package leetcode;
 /**
  * Created by HuaZ on 9/24/2017.
 
- We are given a "tree" in the form of a 2D-array, with distinct values for each node.
+ In this problem, a tree is an undirected graph that is connected and has no cycles.
 
- In the given 2D-array, each element pair [u, v] represents that v is a child of u in the tree.
+ The given input is a graph that started as a tree with N nodes (with distinct values 1, 2, ..., N),
+ with one additional edge added. The added edge has two different vertices chosen from 1 to N,
+ and was not an edge that already existed.
 
- We can remove exactly one redundant pair in this "tree" to make the result a tree.
+ The resulting graph is given as a 2D-array of edges. Each element of edges is a pair [u, v] with u < v,
+ that represents an undirected edge connecting nodes u and v.
 
- You need to find and output such a pair. If there are multiple answers for this question,
- output the one appearing last in the 2D-array. There is always at least one answer.
+ Return an edge that can be removed so that the resulting graph is a tree of N nodes.
+ If there are multiple answers, return the answer that occurs last in the given 2D-array.
+ The answer edge [u, v] should be in the same format, with u < v.
 
  Example 1:
 
  Input: [[1,2], [1,3], [2,3]]
  Output: [2,3]
- Explanation: Original tree will be like this:
+ Explanation: The given undirected graph will be like this:
    1
   / \
  2 - 3
 
  Example 2:
 
- Input: [[1,2], [1,3], [3,1]]
- Output: [3,1]
- Explanation: Original tree will be like this:
-   1
-  / \\
- 2   3
+ Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
+ Output: [1,4]
+ Explanation: The given undirected graph will be like this:
+ 5 - 1 - 2
+     |   |
+     4 - 3
 
  Note:
- The size of the input 2D-array will be between 1 and 1000.
- Every integer represented in the 2D-array will be between 1 and 2000.
+ The size of the input 2D-array will be between 3 and 1000.
+ Every integer represented in the 2D-array will be between 1 and N, where N is the size of the input array.
+
+
+ Update (2017-09-26):
+ We have overhauled the problem description + test cases and specified clearly the graph is an undirected graph.
+ For the directed graph follow up please see Redundant Connection II). We apologize for any inconvenience caused.
 
  */
 public class N684_RedundantConnection {
     // union find to detect first cyclic edge
-    // 38 / 38 test cases passed.
+    // undirected graph
+    // 39 / 39 test cases passed.
     // 7 ms
     class Solution {
         public int[] findRedundantConnection(int[][] edges) {
